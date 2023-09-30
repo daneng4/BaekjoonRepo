344f6517913d8b1ddc6b0b09627de91f995d961f
daneng4 <89981466+daneng4@users.noreply.github.com>
2023-09-30 12:18:44 +0900

    [Silver V] Title: LCM, Time: 124 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static long gcd(int x, int y) {
+        if (x % y == 0)
+            return y;
+        return gcd(y, x % y);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println((long) a * b / gcd(a, b));
+        }
+    }
+}
