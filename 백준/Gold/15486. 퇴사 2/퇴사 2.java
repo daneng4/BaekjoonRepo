3117b7f633bb44644de9e40b88b6b34f588ddf37
daneng4 <89981466+daneng4@users.noreply.github.com>
2023-10-25 18:07:40 +0900

    [Gold V] Title: 퇴사 2, Time: 824 ms, Memory: 345764 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,36 +1,37 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	public static int[][] work;
-	public static int[] priceSum;
-	
+	public static int[][] meeting;
+	public static int[] price;
 	public static void main(String[] args) throws IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		work = new int[n+2][2];
-		priceSum = new int[n+2];
+		meeting = new int[n+2][2];
+		price = new int[n+2];
 		StringTokenizer st;
+		
 		for(int i = 1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int date = Integer.parseInt(st.nextToken());
-			int price = Integer.parseInt(st.nextToken());
-			work[i][0] = date;
-			work[i][1] = price;
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			
+			meeting[i][0] = t;
+			meeting[i][1] = p;
 		}
-
+		
 		int max = -1;
 		for(int i = 1; i<=n+1; i++) {
-			if(max < priceSum[i]) {
-				max = priceSum[i];
+			if(max < price[i])
+				max = price[i];
+			int nextDate = i + meeting[i][0];
+			if(nextDate < n+2) {
+				price[nextDate] = Math.max(price[nextDate], max + meeting[i][1]);
 			}
-			
-			int nextDate = i + work[i][0];
-			if(nextDate < n+2)
-				priceSum[nextDate] = Math.max(priceSum[nextDate], max + work[i][1]);
 		}
-		System.out.println(priceSum[n+1]);
+		System.out.println(price[n+1]);
+		
 	}
 }
