cd0e3f3b91dea96c29e0abb21063fc55a80f97cf
daneng4 <89981466+daneng4@users.noreply.github.com>
2023-11-06 18:38:15 +0900

    [Gold V] Title: 이진 검색 트리, Time: 644 ms, Memory: 29024 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,61 +1,50 @@
 import org.w3c.dom.Node;
 
-import java.io.*;
-import java.sql.Array;
 import java.util.*;
+import java.io.*;
 
-
-public class Main {
+public class Main  {
     static class Node{
-        int node; Node left; Node right;
 
+        int node;
+        Node left;
+        Node right;
         Node(int node){
             this.node = node;
         }
-        Node(int node, Node left, Node right){
-            this.node = node;
-            this.left = left;
-            this.right = right;
+        void insert(int node){
+            if(node < this.node) // 현재 노드보다 매개변수 노드 값이 작은 경우
+                if(this.left == null) // 현재 노드의 left가 null인 경우
+                    this.left = new Node(node); // 매개변수 노드를 현재 노드의 left로 생성
+                else // 현재 노드의 left가 null이 아니면
+                    this.left.insert(node); // 현재 노드의 left에 insert
+            else // 현재 노드보다 매개변수 노드가 큰 경우
+                if(this.right == null) // 현재 노드의 right가 null인 경우
+                    this.right = new Node(node); // 매개변수 노드를 현재 노드의 right로 생성
+                else  // 현재 노드의 right가 null이 아니면
+                    this.right.insert(node); // 현재 노드의 right에 insert
         }
-
-        void insert(int num){
-            if(num < this.node)
-                if(this.left == null)
-                    this.left = new Node(num);
-                else
-                    this.left.insert(num);
-            else{
-                if(this.right == null)
-                    this.right = new Node(num);
-                else
-                    this.right.insert(num);
-
-            }
-        }
-
     }
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Node root = new Node(Integer.parseInt(br.readLine()));
         String str;
+        Node node = new Node(Integer.parseInt(br.readLine()));
         while(true){
             str = br.readLine();
-            if(str == null || str.equals(""))
+            if(str == null || str.equals("")) // 더 이상 입력이 없으면 반복문 종료
                 break;
 
-            root.insert(Integer.parseInt(str));
+            node.insert(Integer.parseInt(str)); // Node class에 입력값 삽입
         }
-        postOrder(root);
+        postOrder(node);
     }
-
-    static void postOrder(Node node){
+    static void postOrder(Node node){ // 후위순회(왼-오-루트)
         if(node == null)
             return;
 
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.node);
+        postOrder(node.left); // 왼쪽
+        postOrder(node.right); // 오른쪽
+        System.out.println(node.node); // 루트
     }
 }
