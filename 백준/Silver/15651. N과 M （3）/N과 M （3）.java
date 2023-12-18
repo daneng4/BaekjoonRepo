b821b71954199083f040e7701b02171069320467
daneng4 <89981466+daneng4@users.noreply.github.com>
2023-12-18 21:12:09 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 68684 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int[] arr;
+	private static boolean[] visit;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		visit = new boolean[n];
+		dfs(n, m, 0);
+		System.out.println(sb);
+		
+	}
+	private static void dfs(int n, int m, int depth) {
+		if(depth == m) {
+			for(int value : arr) {
+				sb.append(value).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0; i<n; i++) {
+			arr[depth] = i+1;
+			dfs(n,m,depth+1);
+		}
+	}
+	
+}
