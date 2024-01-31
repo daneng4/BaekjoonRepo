0d6a3fbefdfc2e7f1bda74bad39594141f03cbf4
김은서 <89981466+daneng4@users.noreply.github.com>
2024-01-31 15:49:47 +0900

    [Silver IV] Title: 주몽, Time: 204 ms, Memory: 16432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,30 +1,42 @@
-import java.io.IOException;
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int cnt = 0;
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[] num = new int[n];
 		
-		
-		int arr[] = new int[N];
-		
-		for(int i =0; i<N; i++) {
-			arr[i] = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i =0; i<N-1; i++) {
-			for(int j =i+1; j<N; j++) {
-				if(arr[i] + arr[j] == M) {
-					cnt = cnt + 1;
-				}
+		Arrays.sort(num);
+		int start = 0;
+		int end = n - 1;
+		int count = 0;
+		
+		while(start < end) {
+			int sum = num[start] + num[end];
+			
+			if(sum < m) {
+				start++;
+			}else if(sum == m) {
+				count++;
+				start++;
+				end--;
+			}else {
+				end--;
 			}
 		}
-		System.out.println(cnt);
+		
+		System.out.println(count);
+		
 	}
-
 }
