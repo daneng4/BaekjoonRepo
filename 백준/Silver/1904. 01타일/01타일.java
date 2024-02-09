14b3f36fd26003153ec81705527afdb1e5752a38
김은서 <89981466+daneng4@users.noreply.github.com>
2024-02-09 23:54:12 +0900

    [Silver III] Title: 01타일, Time: 340 ms, Memory: 56448 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		dp = new int[1000001];
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i<dp.length; i++) {
+			dp[i] = -1;
+		}
+
+		System.out.println(recur(n));
+	}
+	public static int recur(int n) {
+		
+		if(dp[n] == -1)
+			dp[n] = (recur(n-1) + recur(n-2))%15746;
+		
+		return dp[n];
+	}
+}
