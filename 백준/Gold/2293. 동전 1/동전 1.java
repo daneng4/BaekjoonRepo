a521831d3dfd259c3329107e727e3a9920ffddd5
김은서 <89981466+daneng4@users.noreply.github.com>
2024-02-12 16:32:54 +0900

    [Gold V] Title: 동전 1, Time: 136 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] coin = new int[n];
+		int[] dp = new int[k+1];
+		
+		for(int i = 0; i<n; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+		
+		dp[0] = 1;
+		for(int i = 0; i<n; i++) {
+			for(int j = 1; j<=k; j++) {
+				if(j>=coin[i])
+					dp[j] += dp[j-coin[i]];
+			}
+		}
+		
+		System.out.println(dp[k]);
+	}
+}
