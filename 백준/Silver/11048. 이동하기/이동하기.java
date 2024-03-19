a5a0bf4b11d67a69d35556dedad7983f6965cc56
김은서 <89981466+daneng4@users.noreply.github.com>
2024-03-19 17:03:30 +0900

    [Silver II] Title: 이동하기, Time: 476 ms, Memory: 78100 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,52 +1,38 @@
 
-
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-
-	public static void main(String[] args) throws Exception  {
+	static int[][] map;
+	static int[][] dp;
+	static int n,m;
+	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		
-		int [][] A = new int[N][M];
-		int [][] dp = new int[N][M];
+		map = new int[n+1][m+1];
+		dp = new int[n+1][m+1];
 		
-		
-		for(int i=0; i<N; i++) {
+		for(int i = 1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j <M; j++) {
-				A[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 1; j<=m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		dp[0][0] = A[0][0];
-		for(int i=1; i<M; i++) {
-			dp[0][i] = dp[0][i-1] + A[0][i];
-		}
-		
-		for(int r=1; r<N; r++) {
-			for(int c =0; c<M; c++) {
-				dp[r][c] = dp[r-1][c];
-				if(r-1 >=0 && c-1 >=0) {
-					dp[r][c] = Math.max(dp[r][c], dp[r-1][c-1]);
-				}
-				
-				if(c-1 >=0) {
-					dp[r][c] = Math.max(dp[r][c], dp[r][c-1]);
-				}
-				
-				dp[r][c] += A[r][c];
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=m; j++) {
+				dp[i][j] = Math.max(dp[i-1][j], Math.max(dp[i][j-1], dp[i-1][j-1]))
+						+ map[i][j];
 			}
 		}
-		System.out.println(dp[N-1][M-1]);
+		
+		System.out.println(dp[n][m]);
+			
 	}
 
-		
 }
-
-
