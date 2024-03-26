0e0fb99445cbda9fc39e937dcab4d6933adc400b
김은서 <89981466+daneng4@users.noreply.github.com>
2024-03-26 14:55:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,55 +1,64 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
-	public static int[] numbers;
-	public static int[] oper = new int[4];
-	public static int n;
-	public static int max = Integer.MIN_VALUE;
-	public static int min = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException{
+	static int[] num;
+	static int n;
+	static int min = Integer.MAX_VALUE;
+	static int max = Integer.MIN_VALUE;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		numbers = new int[n];
-
-		StringTokenizer st = new StringTokenizer(br.readLine());		
+		num = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<n; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		int[] op = new int[4];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<4; i++) {
-			oper[i] = Integer.parseInt(st.nextToken());
+			op[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		find(numbers[0], 1);
-		
+		dfs(op, num[0], 1);
 		System.out.println(max);
 		System.out.println(min);
 	}
-	public static void find(int num, int idx) {
-		if(idx == n) {
-			max = Math.max(max, num);
-			min = Math.min(min, num);
+	public static void dfs(int[] op, int sum, int depth) {
+		if(depth == n) {
+			max = Math.max(sum, max);
+			min = Math.min(sum, min);
 			return;
 		}
 		
 		for(int i = 0; i<4; i++) {
-			if(oper[i] > 0) {
-				
-				oper[i]--;
-				
-				switch(i) {
-				case 0 : find(num + numbers[idx], idx + 1); break;
-				case 1 : find(num - numbers[idx], idx + 1); break;
-				case 2 : find(num * numbers[idx], idx + 1); break;
-				case 3 : find(num / numbers[idx], idx + 1); break;
+			if(op[i] == 0)
+				continue;
+			
+			op[i]--;
+			// 덧셈이라면
+			if(i == 0)
+				dfs(op, sum + num[depth], depth+1);
+			// 뺄셈이라면
+			else if(i == 1)
+				dfs(op, sum - num[depth], depth+1);
+			// 곱셈이라면
+			else if(i == 2)
+				dfs(op, sum * num[depth], depth+1);
+			// 나눗셈이라면
+			else {
+				// 음수의 나눗셈이라면
+				if(sum < 0) {
+					dfs(op, -(Math.abs(sum) / num[depth]), depth+1);
+				}else {
+					dfs(op, sum / num[depth], depth+1);
 				}
-				oper[i]++;
 			}
-		} // end of loop
+			op[i]++;
+		}
+		
 		
 	}
-	
-
 }
