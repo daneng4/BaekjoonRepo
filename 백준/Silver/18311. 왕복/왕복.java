6e2c35a0b7cf515e7f6f0351d58f4f9af6f49879
김은서 <89981466+daneng4@users.noreply.github.com>
2024-03-28 11:55:42 +0900

    [Silver V] Title: 왕복, Time: 316 ms, Memory: 25460 KB -BaekjoonHub

---
+++ 백준/Silver/18311. 왕복/왕복.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static long[] A;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		long k = Long.parseLong(st.nextToken());
+
+		A = new long[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+			k -= A[i];
+			if (k < 0) {
+				System.out.println(i + 1);
+				return;
+			}
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			k -= A[i];
+			if (k < 0) {
+				System.out.println(i + 1);
+				return;
+			}
+		}
+	}
+}
