7af439a93b8128185b813f40218764042605b861
김은서 <89981466+daneng4@users.noreply.github.com>
2024-04-02 16:29:36 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 836 ms, Memory: 37352 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,61 +1,62 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int n,m,r;
-	public static int min;
-	public static int[][] map;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
-	public static void main(String[] args) throws Exception  {
+	static int n,m,r;
+	static int[][] map;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
+		map = new int[n+1][m+1];
 		
-		for(int i = 0; i<n; i++) {
+		for(int i = 1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<m; j++) {
+			for(int j = 1; j<=m; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		min = Math.min(n, m);
-		for(int i = 0; i<r; i++) {
-			spin();
-		}
 		
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<m; j++) {
-				System.out.print(map[i][j] + " ");
+		for(int k = 0; k<r; k++) {
+			int t = Math.min(n, m) / 2;
+			for(int i = 0; i < t; i++) {
+				turn(1+i, 1+i, n-i, m-i);
 			}
-			System.out.println();
 		}
-		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=m; j++) {
+				sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
 	}
-	public static void spin() {
-		for(int i = 0; i<min/2; i++) {
-			int x = i;
-			int y = i;
-			
-			int temp = map[x][y];
-			int idx = 0;
-			while(idx<4) {
-				int nextx = x + dx[idx];
-				int nexty = y + dy[idx];
-				
-				if(nextx<n-i && nexty<m-i && nextx >= i && nexty >= i) {
-					map[x][y] = map[nextx][nexty];
-					x = nextx;
-					y = nexty;
-				}else {
-					idx++;
-				}
-			}
-			map[i+1][i] = temp;
+	public static void turn(int x, int y, int n, int m) {
+		// 시작은 가장 왼쪽 위에 있는 배열 부터, 위 <- 오른쪽 <- 아래 <- 왼쪽
+		int temp = map[x][y];
+		
+		// 위쪽 이동
+		for(int i = 0; i<m-y; i++) {
+			map[x][y+i] = map[x][y+i+1];
 		}
-
+		// 오른쪽 이동
+		for(int i = 0; i<n-x; i++) {
+			map[x+i][m] = map[x+i+1][m];
+		}
+		// 아래쪽 이동
+		for(int i = 0; i<m-y; i++) {
+			map[n][m-i] = map[n][m-i-1];
+		}
+		// 왼쪽 이동
+		for(int i = 0; i<n-x-1; i++) {
+			map[n-i][y] = map[n-i-1][y];
+		}
+		map[x+1][y] = temp;
 	}
 }
