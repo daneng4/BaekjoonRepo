d4bc9c3f1d888fb8ecf5960fd91942834ecbb4d2
김은서 <89981466+daneng4@users.noreply.github.com>
2024-04-05 10:14:05 +0900

    [Gold IV] Title: 타임머신, Time: 312 ms, Memory: 19596 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -2,37 +2,50 @@
 import java.util.*;
 import java.io.*;
 
+class Node {
+	int start;
+	int end;
+	int cost;
+
+	Node(int start, int end, int cost) {
+		this.start = start;
+		this.end = end;
+		this.cost = cost;
+	}
+}
+
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		
-		ArrayList<edge> list = new ArrayList<>();
+		long max = Long.MAX_VALUE;
 
-		for(int i = 0; i<m; i++) {
+		ArrayList<Node> list = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			list.add(new edge(start, end, cost));
+
+			list.add(new Node(start, end, cost));
 		}
-		
-		long[] dist = new long[n+1];
-		Arrays.fill(dist, Long.MAX_VALUE);
+
+		long[] dist = new long[n + 1];
+		Arrays.fill(dist, max);
 		dist[1] = 0;
-		
-		for(int i = 1; i<=n; i++) {
-			for(edge e : list) {
-				if(dist[e.start] == Long.MAX_VALUE)
+
+		for (int i = 1; i <= n; i++) {
+			for (Node node : list) {
+				if (dist[node.start] == max) 
 					continue;
 				
-				if(dist[e.end] > dist[e.start] + e.cost) {
-					dist[e.end] = dist[e.start] + e.cost; 
+				if(dist[node.end] > dist[node.start] + node.cost) {
+					dist[node.end] = dist[node.start] + node.cost;
 					
 					if(i == n) {
 						System.out.println(-1);
@@ -43,19 +56,11 @@ public class Main {
 		}
 		
 		for(int i = 2; i<=n; i++) {
-			if(dist[i] == Long.MAX_VALUE)
+			if(dist[i] == max)
 				System.out.println(-1);
 			else
 				System.out.println(dist[i]);
 		}
-	}
-}
-class edge{
-	int start;
-	int end;
-	int cost;
-	
-	public edge(int start, int end, int cost) {
-		this.start = start; this.end = end; this.cost = cost;
+
 	}
 }
