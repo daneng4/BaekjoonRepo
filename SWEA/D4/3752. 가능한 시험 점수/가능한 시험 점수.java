6662627172479af4f2e1e9ad9d0c23ec961519d8
김은서 <89981466+daneng4@users.noreply.github.com>
2024-04-17 18:39:25 +0900

    [D4] Title: 가능한 시험 점수, Time: 217 ms, Memory: 51,168 KB -BaekjoonHub

--- SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.java
+++ SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.java
@@ -1,46 +1,49 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 class Solution {
-    static int[] arr;
-    static boolean[][] visited;
-    static int N;
-    
-	public static void main(String args[]) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		  int T = Integer.parseInt(br.readLine());
-	
-		  for(int test_case = 1; test_case <= T; test_case++) {
-            N = Integer.parseInt(br.readLine());
-            String[] input = br.readLine().split(" ");
-            arr = new int[N];
-            int sum = 0;
-            
-            for(int i=0; i<N; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-                sum += arr[i];
-            }
-            visited = new boolean[N+1][sum+1];
-            dfs(0, 0);
-            int ans = 0;
-            for(int i=0; i<=sum; i++) {
-                if(visited[N][i])
-                    ans++;
-            }
-            System.out.println("#"+test_case+" "+ans);
-		  }
-	  }
-    
-    public static void dfs(int idx, int sum) {
-        if(visited[idx][sum]) 
-        	return;  
-        visited[idx][sum] = true;
-        if(idx>=N) 
-        	return;
-        
-        dfs(idx+1, sum+arr[idx]);
-        dfs(idx+1, sum);
-    }
+	static boolean[][] visit;
+	static int n;
+	static int[] num;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			n = Integer.parseInt(br.readLine());
+			num = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sum = 0;
+			for (int i = 0; i < n; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+				sum += num[i];
+			}
+
+			visit = new boolean[n + 1][sum + 1];
+
+			dfs(0, 0);
+			int ans = 0;
+			for (int i = 0; i <= sum; i++) {
+				if (visit[n][i])
+					ans++;
+			}
+			bw.write(String.format("#%d %d\n", test_case, ans));
+		} // end of testcase
+		bw.flush();
+	}
+
+	public static void dfs(int sum, int idx) {
+		if (visit[idx][sum])
+			return;
+		visit[idx][sum] = true;
+		if (idx == n)
+			return;
+		// 이 수를 선택
+		dfs(sum + num[idx], idx + 1);
+		// 선택 x
+		dfs(sum, idx + 1);
+	}
 }
