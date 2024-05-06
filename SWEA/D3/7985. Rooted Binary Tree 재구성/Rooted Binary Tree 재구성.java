8e946589be04b3cc1d40d50dca39b3e404ebf8a0
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-07 00:27:36 +0900

    [D3] Title: Rooted Binary Tree 재구성, Time: 131 ms, Memory: 28,708 KB -BaekjoonHub

--- SWEA/D3/7985. Rooted Binary Tree 재구성/Rooted Binary Tree 재구성.java
+++ SWEA/D3/7985. Rooted Binary Tree 재구성/Rooted Binary Tree 재구성.java
@@ -1,9 +1,9 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int depth;
-	static StringBuilder sb;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,8 +11,7 @@ public class Solution {
 		int testCase = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= testCase; t++) {
 			int k = Integer.parseInt(br.readLine());
-			depth = (int)Math.pow(2, k) - 1;
-			int[] tree = new int[depth+1];
+			int depth = (int)Math.pow(2, k)-1;
 			int[] node = new int[depth];
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,28 +19,31 @@ public class Solution {
 				node[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			sb = new StringBuilder();
-			solve(0, depth-1, 1, 0, tree, node);
+			int[] tree = new int[depth+1];
+			solve(0, depth-1, 1, node, tree);
+			
+			StringBuilder sb = new StringBuilder();
 			int n = 0;
-			int i = 1;
-			for(int level = 0; level < k; level++){
-                n += (int)Math.pow(2, level);
-                for(; i <= n; i++){
-                    sb.append(tree[i]).append(" ");
-                }
-                sb.append("\n");
-            }
+			int j = 1;
+			for(int i = 0; i<k; i++) {
+				n += (int)Math.pow(2, i);
+				for(; j <= n; j++) {
+					sb.append(tree[j]).append(" ");
+				}
+				sb.append("\n");
+			}
 			
 			bw.write(String.format("#%d %s", t, sb.toString()));
 		} // testcase
 		bw.flush();
 	}
-	public static void solve(int left, int right, int tidx, int nidx, int[]tree, int[] node) {
+	public static void solve(int left, int right, int tidx, int[] node, int[] tree) {
 		int root = (left + right) / 2;
 		tree[tidx] = node[root];
 		if(left == right)
 			return;
-		solve(left, root-1, tidx*2, root, tree, node);
-        solve(root+1, right, tidx*2+1, root, tree, node);
+		solve(left, root-1, tidx * 2, node, tree);
+		solve(root+1, right, tidx * 2 + 1, node, tree);
+		
 	}
 }
