76cb409da2f708dfdcff78f1330c2042840eec72
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-14 17:29:21 +0900

    [Silver IV] Title: 수 찾기, Time: 584 ms, Memory: 42996 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,44 +3,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		arr = new int[n];
+		int[] num = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		for(int i = 0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
 		int m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
+		Arrays.sort(num);
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i<m; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(bs(num)<0)
-				sb.append("0").append("\n");
+			int target = Integer.parseInt(st.nextToken());
+			if(find(target, num))
+				sb.append(1 + "\n");
 			else
-				sb.append("1").append("\n");
+				sb.append(0 + "\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
-	public static int bs(int target) {
+	public static boolean find(int target, int[] num) {
 		int start = 0;
-		int end = arr.length -1;
+		int end = num.length-1;
 		
 		while(start <= end) {
 			int mid = (start + end) / 2;
-			if(target > arr[mid])
+			if(num[mid] == target)
+				return true;
+			else if(num[mid] < target) {
 				start = mid+1;
-			else if(target < arr[mid])
+			}else {
 				end = mid-1;
-			else
-				return mid;
+			}
 		}
-		return -1;
+		
+		return false;
 	}
 }
