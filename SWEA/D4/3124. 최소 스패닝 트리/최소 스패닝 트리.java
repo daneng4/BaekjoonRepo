bf206b2802b00af8f2c7e853fb029c0f2c8c46d4
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-15 12:29:29 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,713 ms, Memory: 124,188 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,9 +1,10 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int[] parents;
-	static int v,e;
+	static int v, e;
+	static int[] parent;
 	static ArrayList<Node> list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,10 +16,10 @@ public class Solution {
         	st = new StringTokenizer(br.readLine());
         	v = Integer.parseInt(st.nextToken());
         	e = Integer.parseInt(st.nextToken());
-        	parents = new int[v+1];
+        	parent = new int[v+1];
         	list = new ArrayList<>();
-        	init();
         	
+        	init();
         	for(int i = 0; i<e; i++) {
         		st = new StringTokenizer(br.readLine());
         		int a = Integer.parseInt(st.nextToken());
@@ -29,45 +30,42 @@ public class Solution {
         	}
         	
         	Collections.sort(list);
+        	long answer = 0;
         	int count = 0;
-        	long result = 0;
-        	
-        	for(Node next : list) {
-        		if(!union(next.x, next.y)) {
-        			// 부모가 다르면
-        			result += next.cost;
+        	for(Node n : list) {
+        		if(!union(n.x, n.y)) {
+        			answer += n.cost;
         			if(count + 1 == v-1)
         				break;
         		}
         	}
         	
-        	bw.write(String.format("#%d %d\n", tc, result));
+        	bw.write(String.format("#%d %d\n", tc, answer));
         }// tc
         bw.flush();
     }
     public static void init() {
     	for(int i = 1; i<=v; i++) {
-    		parents[i] = i;
+    		parent[i] = i;
     	}
     }
     public static boolean union(int a, int b) {
-    	int aparent = find(a);
-    	int bparent = find(b);
-    	if(aparent == bparent)
+    	int aroot = find(a);
+    	int broot = find(b);
+    	
+    	if(aroot == broot)
     		return true;
-    	parents[bparent] = aparent;
+    	parent[broot] = aroot;
     	return false;
     }
     public static int find(int x) {
-    	if(parents[x] == x)
+    	if(parent[x] == x)
     		return x;
-    	return parents[x] = find(parents[x]);
+    	return parent[x] = find(parent[x]);
     }
 }
 class Node implements Comparable<Node>{
-	int x;
-	int y;
-	int cost;
+	int x; int y; int cost;
 	Node(int x, int y, int cost){
 		this.x = x; this.y = y; this.cost = cost;
 	}
