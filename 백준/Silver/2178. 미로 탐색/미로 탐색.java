bfb6a5c7da2e43be82fe055a6df163571ff1aaec
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-17 00:27:19 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,61 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	static int[][] map;
-	static boolean[][] visited;
-	static int[] moveX = {1,-1,0,0};
-	static int[] moveY = {0,0,1,-1};
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	static boolean[][] visit;
 	static int n, m;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
 		map = new int[n][m];
-		visited = new boolean[n][m];
+		visit = new boolean[n][m];
 		
-		// map을 2차원 배열로 만들기
 		for(int i = 0; i<n; i++) {
 			String str = br.readLine();
-			char[] mapData = str.toCharArray();
 			for(int j = 0; j<m; j++) {
-				map[i][j] = mapData[j] - '0';
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
-		
+			
 		bfs(0,0);
 		System.out.println(map[n-1][m-1]);
-		
 	}
-	private static void bfs(int x, int y) {
+	public static void bfs(int x, int y) {
 		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {x,y});
+		q.add(new int[] {x,y});
 		
 		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
+			int[] poll = q.poll();
+			int nowx = poll[0];
+			int nowy = poll[1];
+			
+			if(visit[nowx][nowy]) continue;
+			visit[nowx][nowy] = true;
 			
 			for(int i = 0; i<4; i++) {
-				int nextX = nowX + moveX[i];
-				int nextY = nowY + moveY[i];
+				int nextx = nowx + dx[i];
+				int nexty = nowy + dy[i];
 				
-				if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
+				if(nextx < 0 || nexty < 0 || nextx > n-1 || nexty > m-1)
 					continue;
+				
+				if(map[nextx][nexty] == 1 && !visit[nextx][nexty]) {
+					if(map[nextx][nexty] == 1)
+						map[nextx][nexty] = map[nowx][nowy] + 1;
+					else
+						map[nextx][nexty] = Math.min(map[nextx][nexty], map[nowx][nowy] + 1);
+					q.add(new int[] {nextx, nexty});
 				}
 				
-				if(visited[nextX][nextY] || map[nextX][nextY] == 0) {
-					continue;
-				}
-				
-				q.add(new int[] {nextX, nextY});
-				map[nextX][nextY] = map[nowX][nowY] + 1;
-				visited[nextX][nextY] = true;
 			}
+			
 		}
-		
 		
 	}
 }
-
-
