4fd5ab0feaf963bea257bed4ed617d5dcf823727
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-18 17:58:08 +0900

    [Gold V] Title: 줄어드는 수, Time: 156 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -3,32 +3,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N;
-	static int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-	static List<Long> list = new ArrayList<>();
+	static int[] num = {9,8,7,6,5,4,3,2,1,0};
+	static List<Long> list;
+	static int n;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+		list = new ArrayList<>();
 		
-		solve(0,0);
-		list.sort(null);
-		
-		try {
-			System.out.println(list.get(N - 1));
-		} catch (Exception e) {
+		dfs(0,0);
+		Collections.sort(list);
+		if(list.size() < n)
 			System.out.println(-1);
-		}
+		else
+			System.out.println(list.get(n-1));
 	}
-	public static void solve(long num, int inx) {
-		if (!list.contains(num)) {
-			list.add(num);
-		}
-
-		if (inx >= 10) {
+	public static void dfs(long n, int idx) {
+		if(!list.contains(n))
+			list.add(n);
+		// 자릿수가 10이상이면 return
+		if(idx >= 10)
 			return;
-		}
-
-		solve((num * 10 + arr[inx]), inx + 1);
-		solve(num, inx + 1);
+		
+		dfs(n * 10 + num[idx], idx+1);
+		dfs(n, idx+1);
 	}
 }
