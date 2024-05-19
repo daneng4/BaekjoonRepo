04b5c32fef18174230ab88916b4a99d6d718e241
김은서 <89981466+daneng4@users.noreply.github.com>
2024-05-19 12:04:59 +0900

    [Bronze I] Title: 소인수분해, Time: 140 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -2,44 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static boolean[] isPrime;
-	static int max = 10000001;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 최대 값 까지의 소수를 구한다.
-		isPrime = new boolean[max];
 		int n = Integer.parseInt(br.readLine());
-		
-		if(n == 1)
-			return;
-		
-		Arrays.fill(isPrime, true);
-		prime();
-		
 		StringBuilder sb = new StringBuilder();
-		// n이 소수일 때까지 소수들로 나눈다.
-		while(!isPrime[n]) {
-			for(int i = 2; i<=isPrime.length; i++) {
-				if(n % i == 0 && isPrime[i]) {
-					n = n / i;
-					sb.append(i + "\n");
-					break;
-				}
+		
+		for(int i = 2; i<=Math.sqrt(n); i++) {
+			while(n % i == 0) {
+				n = n/i;
+				sb.append(i + "\n");
 			}
 		}
-		sb.append(n);
 		
+		if(n != 1) {
+			sb.append(n);
+		}
 		System.out.println(sb.toString());
-	}
-	public static void prime() {
-		isPrime[0] = isPrime[1] = false;
-		
-		for(int i = 2; i<=Math.sqrt(max); i++) {
-			if(!isPrime[i])
-				continue;
-			for(int j = i*i; j<max; j+=i) {
-				isPrime[j] = false;
-			}
-		}
 	}
 }
