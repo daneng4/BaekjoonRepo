db814388b4628b6744443a88e3c323f2f74579d1
김은서 <89981466+daneng4@users.noreply.github.com>
2024-06-30 12:04:54 +0900

    [Bronze V] Title: 문자열, Time: 132 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i<n; i++) {
+			String str = br.readLine();
+			sb.append(String.format("%s%s\n", str.charAt(0), str.charAt(str.length()-1)));
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
