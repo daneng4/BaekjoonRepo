e8ea91c1ee766a6101d6c95167b25b2532a4639d
김은서 <89981466+daneng4@users.noreply.github.com>
2024-07-11 11:34:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        
+        if(price >= 500000){
+            answer = (int)(price * 0.8);
+            return answer;
+        }else if(price >= 300000){
+            answer = (int)(price * 0.9);
+            return answer;
+        }else if(price >= 100000){
+            answer = (int)(price * 0.95);
+            return answer;
+        }
+        
+        answer = price;
+        return answer;
+    }
+}
