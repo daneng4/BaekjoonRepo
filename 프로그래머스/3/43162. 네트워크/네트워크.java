8d7bc5e3d3253fa7bb0744c5d7eebadd81495d3d
김은서 <89981466+daneng4@users.noreply.github.com>
2024-07-18 14:25:01 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,29 @@
+import java.util.*;
+
 class Solution {
     static boolean[] visit;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visit = new boolean[n];
+
+        visit = new boolean[n+1];
         
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-            dfs(computers, i, visit);
-            answer++;
+        for(int i = 0; i<n; i++){
+            if(!visit[i]){
+                dfs(computers, i);
+                answer++;
+            }
         }
-    }
         
         return answer;
     }
-    public static boolean[] dfs(int[][] computers, int i, boolean[] visit){
+    public static void dfs(int[][] computers, int i){
         visit[i] = true;
-        for (int j = 0; j < computers.length; j++) {
-            if (i != j && computers[i][j] == 1 && visit[j] == false) {
-            visit = dfs(computers, j, visit);
-            }
+        
+        for(int j = 0; j<computers.length; j++){
+            if(i != j && computers[i][j] == 1 && !visit[j])
+                dfs(computers, j);
         }
-        return visit;
+        
     }
 }
