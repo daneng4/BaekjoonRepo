db59858ceedc2d772522cff923d2faa5effd0ff2
김은서 <89981466+daneng4@users.noreply.github.com>
2024-10-22 22:18:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.46 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,24 +1,22 @@
 import java.util.*;
-
+import java.io.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        Map<String, Integer> hash = new HashMap<>();
         
-        // participant 맵에 넣어놓고 completion이 맵에잇는지 확인
-        // 잇으면 value -1, 마지막에 맵에 존재하는 key가 답이된다
-        
-        Map<String, Integer> map = new HashMap<>();
-        
-        for(String p : participant){
-            map.put(p, map.getOrDefault(p, 0) + 1);
-        }
-        for(String c : completion){
-            map.put(c, map.get(c) - 1);
+        for(int i = 0; i<participant.length; i++){
+            hash.put(participant[i], hash.getOrDefault(participant[i], 0) + 1);
         }
         
-        for(String s : map.keySet()){
-            if(map.get(s) == 1)
-                answer = s;
+        for(String comp : completion){
+            hash.put(comp, hash.get(comp)-1);
+        }
+        
+        for(String key : hash.keySet()){
+            if(hash.get(key) > 0){
+                answer = key;
+            }
         }
         
         return answer;
