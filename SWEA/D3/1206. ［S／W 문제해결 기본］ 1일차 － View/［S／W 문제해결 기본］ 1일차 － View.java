9c59274d091b6ff51f2d662310df875c3050ee44
김은서 <89981466+daneng4@users.noreply.github.com>
2024-10-24 23:25:19 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 137 ms, Memory: 23,032 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -2,42 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int[] arr;
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int testCase = 10;
-		for(int t = 1; t<=testCase; t++) {
-			int n = Integer.parseInt(br.readLine());
-			arr = new int[n+1];
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		int tc = 10;
+		StringTokenizer st;
+		
+		for(int t = 1; t<=tc; t++) {
+			int size = Integer.parseInt(br.readLine());
+			int[] h = new int[size];
+			st = new StringTokenizer(br.readLine());
+			int answer = 0;
 			
-			for(int i = 1; i<=n; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+			for(int i = 0; i<size; i++) {
+				h[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			int answer = 0;
-			for(int i = 3; i<=n-2; i++) {
-				answer += check(i);
+			for(int i = 2; i<size-2; i++) {
+				int rs = checkHeight(h, i);
+				answer += rs;
 			}
 			
 			bw.write(String.format("#%d %d\n", t, answer));
 		}
-		
 		bw.flush();
+		bw.close();
 	}
-	public static int check(int x) {
-		if(arr[x-2] >= arr[x]) 
-			return 0;
-		if(arr[x-1] >= arr[x])
-			return 0;
-		if(arr[x+1] >= arr[x])
-			return 0;
-		if(arr[x+2] >= arr[x])
-			return 0;
+	public static int checkHeight(int[] h, int idx) {
+		int cur = h[idx];
+		int max = 0;
+		for(int i = idx-2; i<=idx+2; i++) {
+			if(i == idx) continue;
+			max = Math.max(max, h[i]);
+		}
 		
-		int result = Math.max(arr[x-2], Math.max(arr[x-1], Math.max(arr[x+1], arr[x+2])));
-		return arr[x] - result;
+		return cur > max ? cur-max : 0;
 	}
 }
