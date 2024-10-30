3a639d287748bbc51db6daa6d6d1a38f97389c18
김은서 <89981466+daneng4@users.noreply.github.com>
2024-10-30 21:06:20 +0900

    [D3] Title: N-Queen, Time: 124 ms, Memory: 20,200 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,48 +1,49 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-	static int ans = 0;
-    static int[] chess;
-    static int N;
-	public static void main(String[] args) throws Exception {
+	static int n;
+	static int[] arr;
+	static int answer;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = Integer.parseInt(br.readLine());
-		for(int test_case=1;test_case<=T;test_case++){
-            ans = 0;
-
-            N = Integer.parseInt(br.readLine());
-            chess = new int[N];
-
-            dfs(0);
-
-            bw.write(String.format("#%d %d\n", test_case, ans));
-        }
+		int testcase = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc<=testcase; tc++) {
+			answer = 0;
+			n = Integer.parseInt(br.readLine());
+			arr = new int[n];
+			
+			dfs(0);
+			
+			bw.write(String.format("#%d %d\n", tc, answer));
+		}
 		bw.flush();
 		bw.close();
+	}
+	static void dfs(int depth) {
+		if(depth == n) {
+			answer += 1;
+			return;
+		}
 		
-	}	
-	static void dfs(int depth){
-        if(depth == N){
-            ans++;
-            return;
-        }
-
-        for(int i=0;i<N;i++){
-            chess[depth] = i;
-            if(isPossible(depth)){
-                dfs(depth + 1);
-            }
-        }
-    }
-    static boolean isPossible(int depth){
-        for(int i=0;i<depth;i++){
-            if(chess[i] == chess[depth] ||depth - i == Math.abs(chess[depth]- chess[i])){
-                return false;
-            }
-        }
-        return true;
-    }
+		for(int i = 0; i<n; i++) {
+			arr[depth] = i;
+			if(check(depth)) {
+				dfs(depth + 1);
+			}
+		}
+	}
+	static boolean check(int depth) {
+		for(int i = 0; i<depth; i++) {
+			// arr[depth] == arr[i] : 같은 열 체크
+			// depth - i == Math.abs(arr[depth] - arr[i]) : 대각선 체크
+			if(arr[depth] == arr[i] || depth - i == Math.abs(arr[depth] - arr[i]))
+				return false;
+		}
+		return true;
+	}
 }
