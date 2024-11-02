670e4e38fbfca465d9638139b300e9dd7bb10ff1
김은서 <89981466+daneng4@users.noreply.github.com>
2024-11-02 23:08:24 +0900

    [D3] Title: N-Queen, Time: 138 ms, Memory: 19,948 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,29 +1,28 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int n;
 	static int[] arr;
-	static int answer;
+	static int n, answer;
 	public static void main(String[] args) throws IOException {
+        
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
 		
-		int testcase = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc<=testcase; tc++) {
-			answer = 0;
+		for(int tc = 1; tc<=t; tc++) {
 			n = Integer.parseInt(br.readLine());
 			arr = new int[n];
+			answer = 0;
 			
 			dfs(0);
-			
 			bw.write(String.format("#%d %d\n", tc, answer));
+			
 		}
 		bw.flush();
 		bw.close();
-	}
+		br.close();
+    }
 	static void dfs(int depth) {
 		if(depth == n) {
 			answer += 1;
@@ -32,15 +31,13 @@ public class Solution {
 		
 		for(int i = 0; i<n; i++) {
 			arr[depth] = i;
-			if(check(depth)) {
+			if(possible(depth)) {
 				dfs(depth + 1);
 			}
 		}
 	}
-	static boolean check(int depth) {
-		for(int i = 0; i<depth; i++) {
-			// arr[depth] == arr[i] : 같은 열 체크
-			// depth - i == Math.abs(arr[depth] - arr[i]) : 대각선 체크
+	static boolean possible(int depth) {
+		for(int i = 0; i<depth; i++) {	
 			if(arr[depth] == arr[i] || depth - i == Math.abs(arr[depth] - arr[i]))
 				return false;
 		}
