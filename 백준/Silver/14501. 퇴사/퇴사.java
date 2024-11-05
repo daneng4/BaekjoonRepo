7dea793c0235dc0c28f25e46ccca6f76aea85414
김은서 <89981466+daneng4@users.noreply.github.com>
2024-11-05 15:47:11 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,41 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] arr;
-    static int result;
-    static int n;
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine()); // 일하는 날
-        StringTokenizer st = null;
-        arr = new int[n][2]; // 걸리는 시간 & 수익
-
-        for(int i = 0; i<n; i++) { // 1일부터 시작
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken()); // 상담에 걸리는 시간
-            arr[i][1] = Integer.parseInt(st.nextToken()); // 보수
-        }
-
-        result = 0;
-        dfs(0,0);
-        System.out.println(result);
-
-    }
-    public static void dfs(int day, int pay) {
-        if (day >= n) {
-            // 다 일 했으면 리턴
-            result = Math.max(pay, result);
-            return;
-        }
-
-        // 상담을 끝낼 수 있으면 상담이 끝난 날짜와 보수 넣음
-        if (day + arr[day][0] <= n){
-            dfs(day + arr[day][0], pay + arr[day][1]);
-        }else { // 상담을 못 끝내면 날짜만 넘김
-            dfs(day + arr[day][0], pay);
-        }
-
-        dfs(day+1, pay);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] price = new int[N];
+		int[] task = new int[N];
+		int[] dp = new int[N+1];
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			task[i] = Integer.parseInt(st.nextToken());
+			price[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i<N; i++) {
+			if(i + task[i] <= N) {
+				dp[i + task[i]] = Math.max(dp[i + task[i]], dp[i] + price[i]);
+			}
+			dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+		}
+		
+		System.out.println(dp[N]);
+	}
+	
 }
