01515f7500bf5b5443bf1230e75c579ff79717e4
김은서 <89981466+daneng4@users.noreply.github.com>
2024-11-07 13:11:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 141908 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,59 +1,46 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int[][] graph;
 	static boolean[] visit;
-	static int count = 1;
-	public static void main(String[] args) throws Exception  {
+	static ArrayList<Integer>[] map;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		
-		graph = new int[n+1][n+1];
 		visit = new boolean[n+1];
+		map = new ArrayList[n+1];
+
+		for(int i = 1; i<=n; i++) {
+			map[i] = new ArrayList<>();
+		}
 		
 		for(int i = 0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			//무방향 그래프 정보 입력
-			graph[a][b] = graph[b][a] = 1;
+			map[a].add(b);
+			map[b].add(a);
 		}
 		
+		int answer = 0;
 		for(int i = 1; i<=n; i++) {
-			if(visit[i])
-				continue;
-			
-			// i 노드를 시작으로 연결된 모든 노드 순회
-			dfs(i);
-			for(int j = 1; j<visit.length; j++) {
-				// i 노드를 시작으로 전부 순회했지만 아직 미방문한 노드가 있다면
-				// 다른 연결 정보가 있는 것이므로 count를 증가하고 break
-				if(!visit[j]) {
-					count++;
-					break;
-				}
+			if(!visit[i]) {
+				dfs(i);
+				answer += 1;
 			}
 		}
 		
-		System.out.println(count);
+		System.out.println(answer);
 	}
-	public static void dfs(int node) {
-		visit[node] = true;
-		
-		for(int i = 1; i<graph.length; i++) {
-			// 현재 노드와 연결된 노드이며 아직 방문을 안한 노드인 경우
-			if(graph[node][i] == 1 && !visit[i])
-				dfs(i);
+	static void dfs(int start) {
+		if(visit[start])
+			return;
+		visit[start] = true;
+		for(int next : map[start]) {
+			dfs(next);
 		}
 	}
 }
-
-
-
-
