24e8598f3e2e10a1826105a99a2867f5752ec748
김은서 <89981466+daneng4@users.noreply.github.com>
2024-11-07 14:02:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,19 +1,29 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception  {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+
+		int fiveCount = n / 5;
+		int count = Integer.MAX_VALUE;
+		boolean pos = false;
 		
-		if(n == 4 || n == 7)
+		for(int i = fiveCount; i >= 0; i--) {
+			if(n % 5 == 0) {
+				pos = true;
+				count = Math.min(count, n / 5);
+			}else if((n - (5 * i)) % 3 == 0){
+				pos = true;
+				int d =  (n - (5 * i)) / 3;
+				count = Math.min(count, i + d);
+			}
+		}
+		if(!pos)
 			System.out.println(-1);
-		else if(n % 5 == 0)
-			System.out.println(n/5);
-		else if(n % 5 == 1 || n % 5 == 3)
-			System.out.println(n/5 + 1);
-		else if(n % 5 == 2 || n % 5 == 4)
-			System.out.println(n/5 + 2);
+		else
+			System.out.println(count);
 	}
 }
