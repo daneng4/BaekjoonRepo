0d6e0b0f7ff82ab5225ad7c993f5003c7f0c3117
김은서 <89981466+daneng4@users.noreply.github.com>
2024-11-17 14:58:45 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 188 ms, Memory: 32,236 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,51 +1,67 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	int tc = 10;
-    	
-    	for(int t = 1; t<=tc; t++) {
-    		int[][] map = new int[100][100];
-    		int testNum = Integer.parseInt(br.readLine());
-    		
-    		int answer = 0;
-    		int sum = 0;
-    		for(int i = 0; i<100; i++){
-    			StringTokenizer st = new StringTokenizer(br.readLine());
-    			sum = 0;
-    			for(int j = 0; j<100; j++) {
-    				map[i][j] = Integer.parseInt(st.nextToken());
-    				// 행의 합 구할 수 있다.
-    				sum += map[i][j];
-    			}
-    			answer = Math.max(answer, sum);
-    		}
-    		
-    		for(int i = 0; i<100; i++) {
-    			sum = 0;
-    			for(int j = 0; j<100; j++) {
-    				sum+= map[j][i];
-    			}
-    			answer = Math.max(answer, sum);
-    		}
-    		
-    		sum = 0;
-    		for(int i = 0; i<100; i++) {
-    			sum += map[0+i][0+i];
-    		}
-    		answer = Math.max(answer, sum);
-    		sum = 0;
-    		for(int i = 0; i<100; i++) {
-    			sum += map[0+i][99-i];
-    		}
-    		answer = Math.max(answer, sum);
-    		
-    		bw.write(String.format("#%d %d\n", testNum, answer));
-    	} // end of testcase
-    	bw.flush();
-    }
+	static int[][] map;
+	static int max;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int testcase = 10;
+		
+		for(int tc = 1; tc<=testcase; tc++) {
+			int testNumber = Integer.parseInt(br.readLine());
+			map = new int[100][100];
+			max = 0;
+			
+			for(int i = 0; i<100; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j<100; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i = 0; i<100; i++) {
+				getRowMax(i);
+			}
+			for(int i = 0; i<100; i++) {
+				getColMax(i);
+			}
+			
+			getDaegakMax(0,0);
+			getReverseDaegakMax(99,99);
+			bw.write(String.format("#%d %d\n", testNumber, max));
+		}
+		bw.flush();
+		bw.close();
+	}
+	static void getRowMax(int row) {
+		int sum = 0;
+		for(int i = 0; i<100; i++) {
+			sum += map[row][i];
+		}
+		max = Math.max(max, sum);
+	}
+	static void getColMax(int col) {
+		int sum = 0;
+		for(int i = 0; i<100; i++) {
+			sum += map[i][col];
+		}
+		max = Math.max(max, sum);
+	}
+	static void getDaegakMax(int x, int y) {
+		int sum = 0;
+		for(int i = 0; i<100; i++) {
+			sum += map[x+i][y+i];
+		}
+		max = Math.max(max, sum);
+	}
+	static void getReverseDaegakMax(int x, int y) {
+		int sum = 0;
+		for(int i = 0; i<100; i++) {
+			sum += map[x-i][y-i];
+		}
+		max = Math.max(max, sum);
+	}
 }
