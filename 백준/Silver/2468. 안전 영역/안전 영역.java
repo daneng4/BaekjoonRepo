eecc8d10f4423cb818421c2d5099a976ef16e026
김은서 <89981466+daneng4@users.noreply.github.com>
2025-01-05 13:15:36 +0900

    [Silver I] Title: 안전 영역, Time: 308 ms, Memory: 53800 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,89 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[][] map;
+	static boolean[][] visit;
+	static boolean[][] water;
+	static int n;
+	static int answer = 1;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+    public static void main(String args[]) throws Exception  {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	n = Integer.parseInt(br.readLine());
+    	map = new int[n][n];
+    	visit = new boolean[n][n];
+    	water = new boolean[n][n];
+    	int minH = Integer.MAX_VALUE;
+    	int maxH = 0;
+    	
+    	for(int i = 0; i<n; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		for(int j = 0; j<n; j++) {
+    			map[i][j] = Integer.parseInt(st.nextToken());
+    			minH = Math.min(minH, map[i][j]);
+    			maxH = Math.max(maxH, map[i][j]);
+    		}
+    	}
+    	
+    	for(int i = minH; i<=maxH; i++) {
+    		getSafeArea(i);
+    		int count = 0;
+    		
+    		for(int j = 0; j<n; j++) {
+    			for(int k = 0; k<n; k++) {
+    				water[j][k] = visit[j][k];
+    			}
+    		}
+    		
+    		for(int j = 0; j<n; j++) {
+    			for(int k = 0; k<n; k++) {
+    				if(!water[j][k]) {
+    					bfs(j,k, water);   
+    					count+=1;
+    				}
+    			}
+    		}
+    		
+    		answer = Math.max(answer, count);
+    	}
+    	
+    	System.out.println(answer);
+    }
+    public static void getSafeArea(int h) {
+    	for(int i = 0; i<n; i++) {
+    		for(int j = 0; j<n; j++) {
+    			if(map[i][j] <= h)
+    				visit[i][j] = true;
+    		}
+    	}
+    }
+    public static void bfs(int x, int y, boolean[][] water) {
+    	Queue<int[]> q = new LinkedList<>();
+    	q.add(new int[] {x,y});
+    	water[x][y] = true;
+    	
+    	while(!q.isEmpty()) {
+    		int[] cur = q.poll();
+    		int curx = cur[0]; 
+    		int cury = cur[1];
+    		
+    		for(int i = 0; i<4; i++) {
+    			int nextx = curx + dx[i];
+    			int nexty = cury + dy[i];
+    			
+    			if(nextx < 0 || nextx >= n || nexty < 0 || nexty >= n)
+    				continue;
+    			if(water[nextx][nexty])
+    				continue;
+    			
+    			q.add(new int[] {nextx, nexty});
+    			water[nextx][nexty] = true;
+    		}
+    	}
+    	
+    }
+}
