22044ae87116ed7f5d5ecdadadbbde7e0a12e8d0
김은서 <89981466+daneng4@users.noreply.github.com>
2025-02-17 17:13:10 +0900

    [Gold III] Title: 순회강연, Time: 280 ms, Memory: 20556 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,51 +1,49 @@
+import java.io.*;
 import java.util.*;
 
-class Lecture implements Comparable<Lecture> {
-    int pay;
-    int day;
-
-    Lecture(int pay, int day) {
-        this.pay = pay;
-        this.day = day;
-    }
-
-    @Override
-    public int compareTo(Lecture other) {
-        return this.day - other.day; // 마감일을 기준으로 오름차순 정렬
-    }
-}
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Lecture[] lectures = new Lecture[n];
-
-        for (int i = 0; i < n; i++) {
-            int pay = sc.nextInt();
-            int day = sc.nextInt();
-            lectures[i] = new Lecture(pay, day);
-        }
-
-        // 마감일을 기준으로 강연 정렬
-        Arrays.sort(lectures);
-
-        // 우선순위 큐 (최소 힙) 사용
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (Lecture lecture : lectures) {
-            pq.offer(lecture.pay); // 현재 강연의 강연료를 큐에 추가
-            if (pq.size() > lecture.day) {
-                pq.poll(); // 큐의 크기가 마감일보다 크면 가장 작은 값 제거
-            }
-        }
-
-        // 우선순위 큐에 남아있는 강연료의 합 계산
-        int totalPay = 0;
-        while (!pq.isEmpty()) {
-            totalPay += pq.poll();
-        }
-
-        System.out.println(totalPay);
-    }
+	public static void main(String args[]) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(br.readLine());
+		Schedule[] s = new Schedule[n];
+		
+		for(int i = 0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int pay = Integer.parseInt(st.nextToken());
+			int day = Integer.parseInt(st.nextToken());
+			
+			s[i] = new Schedule(day, pay);
+		}
+		
+		Arrays.sort(s);
+		int answer = 0;
+		boolean[] check = new boolean[10001];
+		for(int i = 0; i<n; i++) {
+			for(int j = s[i].day; j>=1; j--) {
+				if(!check[j]) {
+					check[j] = true;
+					answer += s[i].pay;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
+class Schedule implements Comparable<Schedule>{
+	int day;
+	int pay;
+	public Schedule(int day, int pay) {
+		this.day = day;
+		this.pay = pay;
+	}
+	public int compareTo(Schedule s) {
+		if(this.pay == s.pay) {
+			return s.day - this.day;
+		}
+		return s.pay - this.pay;
+	}
 }
