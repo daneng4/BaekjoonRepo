4cd13d24bfbe478209988289021963d8cb0141c0
김은서 <89981466+daneng4@users.noreply.github.com>
2025-03-03 16:38:13 +0900

    [Gold V] Title: 트리, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,70 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-import static java.lang.System.exit;
-
 public class Main {
-    static ArrayList<Integer>[] tree;
-    static int leafNode = 0;
-    static int deleteNode = 0;
-    public static void main(String[] args) throws IOException {
-
+    private static ArrayList<Integer>[] nodes;
+    private static int answer = 0;
+    private static int deleteNode = 0;
+    public static void main(String args[]) throws Exception  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        tree = new ArrayList[n];
+        nodes = new ArrayList[n];
 
-        int rootNode = 0;
-
-        for(int i = 0; i<n; i++){
-            tree[i] = new ArrayList<>();
+        for(int i = 0; i<n; i++) {
+            nodes[i] = new ArrayList<>();
         }
 
+        int root = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            if(num == -1) // -1을 입력받은 인덱스가 루트노드
-                rootNode = i;
-            else
-                tree[num].add(i);
-        }
-        deleteNode = Integer.parseInt(br.readLine()); // 삭제 노드
+        for(int i = 0; i<n; i++) {
+            int node = Integer.parseInt(st.nextToken());
+            if(node == -1) {
+                root = i;
+                continue;
+            }
 
-        if(deleteNode == rootNode){
+            nodes[node].add(i);
+        }
+
+        deleteNode = Integer.parseInt(br.readLine());
+        if(deleteNode == root) {
             System.out.println(0);
-            exit(0);
-        }
-        delete(rootNode);
-        dfs(rootNode);
-
-        System.out.println(leafNode);
-
-    }
-
-    static void dfs(int node){
-//        if(node == deleteNode) { // 해당 노드가 삭제 노드일 경우
-//            return;
-//        }
-        int size = tree[node].size();
-        if(size == 0) // 노드 내에 값이 없다 = 리프 노드
-            leafNode+=1;
-        for(int i = 0; i<size; i++){
-            int nextNode = tree[node].get(i);
-            dfs(nextNode);
-        }
-    }
-
-    static void delete(int node){
-        int size = tree[node].size();
-        if(size == 0)
             return;
+        }
+
+        delete(root);
+        dfs(root);
+
+        System.out.println(answer);
+    }
+    private static void dfs(int node) {
+        int size = nodes[node].size();
+        if(size == 0) {
+            answer += 1;
+            return;
+        }
 
         for(int i = 0; i<size; i++){
-            int nextNode = tree[node].get(i);
-            if(nextNode==deleteNode){
-                tree[node].remove(i);
+            int next = nodes[node].get(i);
+            dfs(next);
+        }
+    }
+
+    private static void delete(int node){
+        int size = nodes[node].size();
+        if(size == 0){
+            return;
+        }
+
+        for(int i = 0; i<size; i++){
+            int next = nodes[node].get(i);
+            if(next == deleteNode){
+                nodes[node].remove(i);
                 break;
             }else{
-                delete(nextNode);
+                delete(next);
             }
         }
     }
