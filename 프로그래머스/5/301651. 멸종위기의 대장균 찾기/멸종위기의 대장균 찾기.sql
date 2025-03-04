41a71c79a3aa72400ea50fe064e06671ce344a15
김은서 <89981466+daneng4@users.noreply.github.com>
2025-03-04 13:53:32 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,21 @@
+-- 코드를 작성해주세요
+with recursive tmp as (
+    #non recursive 문장
+    select id, parent_id, 1 as generation
+    from ecoli_data
+    where parent_id is null
+    union all
+    #recursive 문장
+    select s.id, s.parent_id, tmp.generation + 1
+    from tmp join ecoli_data s
+    on tmp.id = s.parent_id
+)
+
+select count(*) as count, generation
+from tmp
+where id not in (
+        select distinct parent_id
+        from tmp
+        where parent_id is not null)
+group by generation
+order by 2;
