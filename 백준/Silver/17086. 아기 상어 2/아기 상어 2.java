3cfd519a740653b7bef6e4cc9491acb87129126d
김은서 <89981466+daneng4@users.noreply.github.com>
2025-03-11 17:00:53 +0900

    [Silver II] Title: 아기 상어 2, Time: 116 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -4,17 +4,10 @@ import java.util.*;
 public class Main {
     static int n,m;
     static int[][] map;
-    static class Info{
-        int[] pos;
-        int distance;
-        public Info(int[] pos, int distance){
-            this.pos = pos;
-            this.distance = distance;
-        }
-    }
-    static boolean[][] visit;
+    static int[][] dist;
     static int[] dx = {1,-1,0,0,1,1,-1,-1};
     static int[] dy = {0,0,1,-1,1,-1,1,-1};
+    static Queue<int[]> q;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,60 +16,48 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        visit = new boolean[n][m];
-        int[][] safeDistanceArr = new int[n][m];
+        dist = new int[n][m];
+        q = new LinkedList<>();
 
         for(int i  = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int answer = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(map[i][j] == 1)
-                    continue;
-                int result = bfs(new int[] {i,j});
-                safeDistanceArr[i][j] = result;
-                visit = new boolean[n][m];
-            }
-        }
-
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                answer = Math.max(safeDistanceArr[i][j], answer);
-            }
-        }
-
-        System.out.println(answer);
-    }
-    private static int bfs(int[] start){
-        int safeDistance = Integer.MAX_VALUE;
-        Queue<Info> info = new LinkedList<>();
-        info.add(new Info(start, 0));
-        visit[start[0]][start[1]] = true;
-
-        while(!info.isEmpty()){
-            Info now = info.poll();
-            int nowx = now.pos[0];
-            int nowy = now.pos[1];
-            if(map[nowx][nowy] == 1){
-                safeDistance = Math.min(safeDistance, now.distance);
-            }
-
-            for(int i = 0; i<8; i++){
-                int nextx = nowx + dx[i];
-                int nexty = nowy + dy[i];
-
-                if(nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && !visit[nextx][nexty]){
-                    info.add(new Info(new int[] {nextx, nexty}, now.distance + 1));
-                    visit[nextx][nexty] = true;
+                if(map[i][j] == 1){
+                    dist[i][j] = 0;
+                    q.add(new int[] {i,j,0});
+                }else{
+                    dist[i][j] = -1;
                 }
             }
         }
 
-        return safeDistance;
+        bfs();
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                max = Math.max(max, dist[i][j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+    private static void bfs(){
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            int ndist = now[2];
+
+            for(int i = 0; i<8; i++){
+                int nextx = x + dx[i];
+                int nexty = y + dy[i];
+
+                if(nextx >= 0 && nextx < n && nexty >= 0 && nexty < m && dist[nextx][nexty] == -1){
+                    dist[nextx][nexty] = ndist + 1;
+                    q.add(new int[] {nextx, nexty, ndist + 1});
+                }
+            }
+        }
     }
 }
