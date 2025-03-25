12d85d6e3a9d19dab39109f2b2511ce725a02da4
김은서 <89981466+daneng4@users.noreply.github.com>
2025-03-25 11:30:18 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1148 ms, Memory: 92072 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -24,22 +24,19 @@ public class Main {
         System.out.println(sb);
     }
     static int w(int a, int b, int c){
-        if(inRange(a,b,c) && dp[a][b][c] != 0){
-            return dp[a][b][c];
-        }
-
         if(a <= 0 || b <= 0 || c <= 0)
             return 1;
 
         if(a > 20 || b > 20 || c > 20)
             return dp[20][20][20] = w(20, 20, 20);
 
+        if(dp[a][b][c] != 0){
+            return dp[a][b][c];
+        }
+
         if(a < b && b < c)
             return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
 
         return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
-    }
-    static boolean inRange(int a, int b, int c) {
-        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
     }
 }
