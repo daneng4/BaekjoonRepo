30a5abbf43639aa83c44d400510e31989009f1d6
김은서 <89981466+daneng4@users.noreply.github.com>
2025-03-27 16:46:51 +0900

    [Silver II] Title: 창고 다각형, Time: 112 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -2,58 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception  {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[1001];
         StringTokenizer st;
 
+        int[] data = new int[1001];
         int start = Integer.MAX_VALUE;
         int end = 0;
+
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            int l = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            int H = Integer.parseInt(st.nextToken());
 
-            start = Math.min(start, l);
-            end = Math.max(end, l);
+            start = Math.min(start, L);
+            end = Math.max(end, L);
 
-            arr[l] = h;
+            data[L] = H;
         }
 
         Stack<Integer> height = new Stack<>();
-        int temp = arr[start];
-        for(int i = start + 1; i<= end; i++){
-            if(arr[i] < temp){
+        int temp = data[start];
+        for(int i = start + 1; i<=end; i++){
+            // 현재 높이가 temp보다 작은 경우
+            if(data[i] < temp){
                 height.push(i);
-            }else{
+            }
+            // 현재 높이가 temp보다 크거나 같은 경우
+            else{
                 while(!height.isEmpty()){
                     int x = height.pop();
-                    arr[x] = temp;
+                    data[x] = temp;
                 }
-                temp = arr[i];
+                temp = data[i];
             }
         }
 
         height.clear();
-
-        temp = arr[end];
-        for(int i = end-1; i >= start; i--){
-            if(arr[i] < temp)
+        temp = data[end];
+        for(int i = end - 1; i>= start; i--){
+            if(data[i] < temp)
                 height.push(i);
-            else{
-                while(!height.isEmpty()){
+            else {
+                while (!height.isEmpty()) {
                     int x = height.pop();
-                    arr[x] = temp;
+                    data[x] = temp;
                 }
-                temp = arr[i];
+                temp = data[i];
             }
         }
 
         int answer = 0;
         for(int i = start; i<=end; i++)
-            answer += arr[i];
+            answer += data[i];
 
         System.out.println(answer);
     }
