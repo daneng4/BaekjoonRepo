ca65d476c714b0aa3681309f23344415a0228a33
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-02 17:59:15 +0900

    [Gold V] Title: 시간 구간 다중 업데이트 다중 합, Time: 448 ms, Memory: 61184 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Gold/25827. 시간 구간 다중 업데이트 다중 합/시간 구간 다중 업데이트 다중 합.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 23시 59분 59초 -> 4,971,599초
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int max = 4971599;
+        int[] second = new int[max+1];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            String[] start = st.nextToken().split(":");
+            String[] end = st.nextToken().split(":");
+
+            int sSecond = convert(start);
+            int eSecond = convert(end);
+
+            if(op == 1){
+                for(int j = sSecond; j<eSecond; j++){
+                    second[j] += 1;
+                }
+            }else{
+                int sum = 0;
+                for(int j = sSecond; j<eSecond; j++){
+                    sum += second[j];
+                }
+                sb.append(sum).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+    public static int convert(String[] strs) {
+        int second = 0;
+
+        second += Integer.parseInt(strs[0]) * 60 * 60;
+        second += Integer.parseInt(strs[1]) * 60;
+        second += Integer.parseInt(strs[2]);
+
+        return second;
+    }
+}
