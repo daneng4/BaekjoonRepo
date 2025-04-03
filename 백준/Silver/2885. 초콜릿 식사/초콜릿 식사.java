7435c4e09281e3b1d7255c0e20e80a46d4f90465
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-03 10:26:14 +0900

    [Silver II] Title: 초콜릿 식사, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -1,43 +1,34 @@
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int k = Integer.parseInt(br.readLine());
-		int choco = 1;
-		
-		while(choco < k) {
-			choco = choco*2;
-		}
-		System.out.print(choco);
-		int sum = 0;
-		int count = 0;
-		if(k == choco) { // 만약 K개와 산 초코의 정사각형 개수가 같으면 굳이 나눌필요없다.
-			System.out.println(" " + 0);
-		}
-		else { // 다르다면
-			while(choco > 1) {
-				count++;
-				choco = choco/2;
-				sum = sum + choco;
-				if(sum == k) {
-					break;
-				}
-				else if(sum > k) {
-					sum = sum - choco;;
-				}
-				
-			}
-		System.out.println(" "+ count);
-		}
-		
-	}
+        int k = Integer.parseInt(br.readLine());
+        int size = 1;
+        while(size < k){
+            size *= 2;
+        }
+        sb.append(size).append(" ");
+
+        if(k == size){
+            sb.append(0);
+        }else{
+            int count = 0;
+            int curSize = 0;
+            while(curSize != k){
+                size /= 2;
+                curSize += size;
+                if(curSize > k){
+                    curSize -= size;
+                }
+                count++;
+            }
+            sb.append(count);
+        }
+
+        System.out.println(sb);
+    }
 }
-
