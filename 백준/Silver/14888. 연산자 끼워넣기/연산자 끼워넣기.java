63d9f4fcd52df37494a50400acaffcb6d640099f
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-07 13:31:07 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 120 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,64 +1,65 @@
-import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
-	static int[] num;
-	static int n;
-	static int min = Integer.MAX_VALUE;
-	static int max = Integer.MIN_VALUE;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		num = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<n; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] op = new int[4];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<4; i++) {
-			op[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		dfs(op, num[0], 1);
-		System.out.println(max);
-		System.out.println(min);
-	}
-	public static void dfs(int[] op, int sum, int depth) {
-		if(depth == n) {
-			max = Math.max(sum, max);
-			min = Math.min(sum, min);
-			return;
-		}
-		
-		for(int i = 0; i<4; i++) {
-			if(op[i] == 0)
-				continue;
-			
-			op[i]--;
-			// 덧셈이라면
-			if(i == 0)
-				dfs(op, sum + num[depth], depth+1);
-			// 뺄셈이라면
-			else if(i == 1)
-				dfs(op, sum - num[depth], depth+1);
-			// 곱셈이라면
-			else if(i == 2)
-				dfs(op, sum * num[depth], depth+1);
-			// 나눗셈이라면
-			else {
-				// 음수의 나눗셈이라면
-				if(sum < 0) {
-					dfs(op, -(Math.abs(sum) / num[depth]), depth+1);
-				}else {
-					dfs(op, sum / num[depth], depth+1);
-				}
-			}
-			op[i]++;
-		}
-		
-		
-	}
+    static int[] arr;
+    static int[] op;
+    static int[] seq;
+    static int n;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        op = new int[4];
+        seq = new int[n-1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<4; i++){
+            op[i] = Integer.parseInt(st.nextToken());
+        }
+
+        run(0);
+        System.out.println(max);
+        System.out.println(min);
+    }
+    static void run(int idx){
+        if(idx == n-1){
+            cal();
+            return;
+        }
+
+        for(int i = 0; i<4; i++){
+            if(op[i] == 0)
+                continue;
+
+            op[i]--;
+            seq[idx] = i;
+            run(idx + 1);
+            op[i]++;
+        }
+    }
+    static void cal(){
+        int result = arr[0];
+        for(int i = 0; i<n-1; i++){
+            if(seq[i] == 0){
+                result += arr[i+1];
+            }else if(seq[i] == 1){
+                result -= arr[i+1];
+            }else if(seq[i] == 2){
+                result *= arr[i+1];
+            }else{
+                result /= arr[i+1];
+            }
+        }
+
+        max = Math.max(result, max);
+        min = Math.min(result, min);
+    }
 }
