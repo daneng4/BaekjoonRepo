a63f4ce8f4aaeb4707cd78c01736cb780474d667
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-12 15:26:55 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-select o.animal_id, o.name
-from animal_outs o left outer join animal_ins i
+SELECT o.animal_id, o.name
+from animal_outs o left join animal_ins i
 on o.animal_id = i.animal_id
 where i.animal_id is null
-order by animal_id;
+order by animal_id
