69a69e563ee4616b2e08de95f415db6f1c681452
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-15 13:30:58 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,23 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double[] nums = new double[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double max = 0;
+        for(int i = 0; i<n; i++){
+            nums[i] = Double.parseDouble(st.nextToken());
+            max = Math.max(nums[i], max);
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		double[] arr = new double[n];
-		for(int i = 0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		int max = (int)arr[arr.length-1];
-		double sum = 0;
-		for(int i = 0; i<n; i++) {
-			sum += arr[i]/max*100;
-		}
-		
-		System.out.println(sum / n);
-		
-	}
+        double sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += nums[i] / max * 100;
+        }
 
+        System.out.println(sum/n);
+    }
 }
