449aaca064c81b124e644c28d31faaf55bc77441
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-16 18:45:44 +0900

    [Silver II] Title: DNA 비밀번호, Time: 264 ms, Memory: 20492 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,61 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int answer = 0;
-	private static int[] minArr;
-	public static void main(String args[]) throws Exception  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int S = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-		String dna = br.readLine();
-		minArr = new int[4];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<4; i++) {
-			minArr[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        String dna = br.readLine();
+        int[] need = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<4; i++){
+            need[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = start + p - 1;
+        for(int i = start; i<=end; i++){
+            char c = dna.charAt(i);
+            if(c == 'A'){
+                need[0]--;
+            }else if(c == 'C'){
+                need[1]--;
+            }else if(c == 'G'){
+                need[2]--;
+            }else{
+                need[3]--;
+            }
+        }
+
+        int answer = 0;
+        if(check(need))
+            answer++;
+
+		while (end < s - 1) {
+
+			char sc = dna.charAt(start);
+			if (sc == 'A') {
+				need[0]++;
+			} else if (sc == 'C') {
+				need[1]++;
+			} else if (sc == 'G') {
+				need[2]++;
+			} else {
+				need[3]++;
+			}
+			start++;
+			end++;
+
+			sc = dna.charAt(end);
+			if (sc == 'A') {
+				need[0]--;
+			} else if (sc == 'C') {
+				need[1]--;
+			} else if (sc == 'G') {
+				need[2]--;
+			} else {
+				need[3]--;
+			}
+
+			if (check(need)) {
+				answer++;
+			}
 		}
-		
-		for(int i = 0; i<P; i++) {
-			char c = dna.charAt(i);
-			if(c == 'A') minArr[0]--;
-			if(c == 'C') minArr[1]--;
-			if(c == 'G') minArr[2]--;
-			if(c == 'T') minArr[3]--;
-		}
-		
-		checkCondition();
-		int i = -1;
-		
-		for(int j = P; j<S; j++) {
-			// 이전 부분 문자열의 시작 위치
-			i = j - P;
-			
-			char pre = dna.charAt(i);
-			if(pre == 'A') minArr[0]++;
-			if(pre == 'C') minArr[1]++;
-			if(pre == 'G') minArr[2]++;
-			if(pre == 'T') minArr[3]++;
-			
-			char next = dna.charAt(j);
-			if(next == 'A') minArr[0]--;
-			if(next == 'C') minArr[1]--;
-			if(next == 'G') minArr[2]--;
-			if(next == 'T') minArr[3]--;
-			
-			checkCondition();
-		}
-		
-		System.out.println(answer);
-	}
-	private static void checkCondition() {
-		for(int i = 0; i<4; i++) {
-			if(minArr[i] > 0)
-				return;
-		}
-		
-		answer += 1;
-		return;
-	}
+
+        System.out.println(answer);
+    }
+    static boolean check(int[] need){
+        for(int i = 0; i<4; i++){
+            if(need[i] > 0)
+                return false;
+        }
+        return true;
+    }
 }
