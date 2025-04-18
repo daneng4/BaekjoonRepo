6c5507948cc758e9b8bbd961b926a2670dba14ef
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-18 16:53:19 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,45 +1,25 @@
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); //데이터 개수
-		int time[] = new int[n];
-		int sum[] = new int[n];
-		for(int i = 0; i<n; i++) {
-			time[i] = sc.nextInt();
+		Arrays.sort(arr);
+		for(int i = 1; i<n; i++){
+			arr[i] += arr[i-1];
 		}
-		// time을 오름차순 정렬을 했을때가 가장 시간비용이 적다.
-		for(int i = 1; i<n; i++) {
-			int insert_point = i; // 정렬 대상 index
-			int insert_value = time[i]; // 정렬 대상 data
-			
-			for(int j = i-1; j>=0; j--) {
-				if (time[j] < time[i]) {
-					insert_point = j+1;
-					break;
-				}
-				
-				if(j == 0) {
-					insert_point = 0;
-				}
-			}
-			for(int j = i; j > insert_point; j--) { //배열 shift
-				time[j] = time[j-1];
-			}
-			time[insert_point] = insert_value;
+		int sum = 0;
+		for(int i = 0; i<n; i++){
+			sum += arr[i];
 		}
-		sum[0] = time[0];
-		for(int i = 1; i<n; i++) {
-			sum[i] = sum[i-1] + time[i];
-		}
-		int total_time = 0;
-		for(int i = 0; i <n; i++) {
-			total_time += sum[i];
-		}
-		System.out.println(total_time);
-	}
+
+		System.out.println(sum);
+    }
 }
-
