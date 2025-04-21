9c6170f3192cbbb8a999c0a0a0537e06ec8c0d73
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-21 21:22:18 +0900

    [Silver V] Title: K번째 수, Time: 1716 ms, Memory: 370292 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -9,13 +9,42 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		ArrayList<Integer> list = new ArrayList<>();
+		int[] arr = new int[n];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<n; i++){
-			list.add(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		Collections.sort(list);
-		System.out.println(list.get(k-1));
+		quickSort(arr);
+		System.out.println(arr[k-1]);
     }
+	static void quickSort(int[] arr){
+		quickSort(arr, 0, arr.length-1);
+	}
+	static void quickSort(int[] arr, int start, int end){
+		int part2 = partition(arr, start, end);
+		if(start < part2 -1){
+			quickSort(arr, start, part2 -1);
+		}
+		if(part2 < end){
+			quickSort(arr, part2, end);
+		}
+	}
+	static int partition(int[] arr, int start, int end){
+		int pivot = arr[(start + end) / 2];
+		while(start <= end){
+			while(arr[start] < pivot) start++;
+			while(arr[end] > pivot) end--;
+			if(start <= end){
+				swap(arr, start, end);
+				start++;
+				end--;
+			}
+		}
+		return start;
+	}
+	static void swap(int[] arr, int start, int end){
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+	}
 }
