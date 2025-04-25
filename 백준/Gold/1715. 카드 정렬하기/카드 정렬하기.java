7a0ccb78bd2cc00d6a8eae8d1b284ccd2d4eb35f
김은서 <89981466+daneng4@users.noreply.github.com>
2025-04-25 15:16:36 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 320 ms, Memory: 25504 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,31 +1,26 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception  {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		for(int i = 0; i<n; i++) {
-			 pq.add(Integer.parseInt(br.readLine()));
-		}
-		int result = 0;
-		if (n == 1) {
-			System.out.println(0);
-		}else {
-			while(true) {
-				int a = pq.poll();
-				int b = pq.poll();
-				int sum = a+b;
-				result+=sum;
-				if(!pq.isEmpty())
-					pq.offer(sum);
-				else
-					break;
-			}
-			System.out.println(result);
+		for(int i = 0; i<n; i++){
+			pq.add(Integer.parseInt(br.readLine()));
 		}
 
+		int answer = 0;
+		while(!pq.isEmpty()){
+			int poll = pq.poll();
+			if(pq.peek() == null)
+				break;
+
+			int sum = poll + pq.poll();
+			answer += sum;
+			pq.add(sum);
+		}
+
+		System.out.println(answer);
 	}
 }
