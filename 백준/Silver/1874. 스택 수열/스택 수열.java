8797f3820d9a946a4ed83cfeb083648f14373ab3
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-02 15:55:59 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27240 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,43 +1,47 @@
-import java.util.Scanner;
-import java.util.Stack;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();	// 출력할 결과물 저장
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
-		
-		int N = in.nextInt();
-		
-		int start = 0;
-		
-		// N 번 반복
-		while(N -- > 0) {
-			
-			int value = in.nextInt();
-			
-			if(value > start) {
-				// start + 1부터 입력받은 value 까지 push를 한다.
-				for(int i = start + 1; i <= value; i++) {
-					stack.push(i);
-					sb.append('+').append('\n');	// + 를 저장한다. 
+		StringBuilder sb = new StringBuilder();
+		int top = 1;
+		boolean flag = false;
+
+		while(n-- > 0){
+			int num = Integer.parseInt(br.readLine());
+			// 일단 삽입
+			for(int i = top; i<=num; i++){
+				stack.push(i);
+				sb.append("+\n");
+				top++;
+			}
+
+			// num 찾을때까지 stack pop
+			while(true){
+				if(stack.isEmpty()){
+					flag = true;
+					break;
 				}
-				start = value; 	// 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화 
+				if(stack.peek() == num){
+					stack.pop();
+					sb.append("-\n");
+					break;
+				}else{
+					stack.pop();
+					sb.append("-\n");
+				}
 			}
-			
-			// top에 있는 원소가 입력받은 값과 같이 않은 경우  
-			else if(stack.peek() != value) {
-				System.out.println("NO");
-				return;		// 또는 System.exit(0); 으로 대체해도 됨. 
-			}
-			
-			stack.pop();
-			sb.append('-').append('\n');
-			
+
+			if(flag)
+				break;
 		}
-		
-		System.out.println(sb);
+
+		if(flag)
+			System.out.println("NO");
+		else
+			System.out.println(sb);
 	}
 }
