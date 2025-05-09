65a088e90511b1126bc2649863f8f9432203f4c7
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-09 16:04:43 +0900

    [Gold IV] Title: 플로이드, Time: 396 ms, Memory: 44356 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,55 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	public static void main(String[] args) throws Exception  {
-		final int init = 987654321;
+	static final int INF = 999999999;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
+		int[][] map = new int[n+1][n+1];
 
-		int[][] arr = new int[n+1][n+1];
-		
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1; j<=n; j++) {
-				arr[i][j] = init;
-				if(i==j)
-					arr[i][j] = 0;
+		for(int i = 1; i<=n; i++){
+			for(int j = 1; j<=n; j++){
+				if(i == j) continue;
+				map[i][j] = INF;
 			}
 		}
-		
-		for(int i = 0; i<m; i++) {
+
+		for(int i = 0; i<m; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			arr[a][b] = Math.min(arr[a][b], v);
+			int cost = Integer.parseInt(st.nextToken());
+
+			map[a][b] = Math.min(map[a][b], cost);
 		}
-		
-		for(int k = 1; k<=n; k++) {
-			for(int i = 1; i<=n; i++) {
-				for(int j = 1; j<=n; j++) {
-					if(arr[i][j] > arr[i][k] + arr[k][j])
-						arr[i][j] = arr[i][k] + arr[k][j];
-					
+
+		for(int k = 1; k<=n; k++){
+			for(int i = 1; i<=n; i++){
+				for(int j = 1; j<=n; j++){
+					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
 				}
 			}
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1; j<=n; j++ ) {
-				if(arr[i][j] == init)
-					arr[i][j] = 0;
-				
-				sb.append(arr[i][j]).append(" ");
+		for(int i = 1; i<=n; i++){
+			for(int j = 1; j<=n; j++){
+				if(map[i][j] == INF) sb.append(0 + " ");
+				else sb.append(map[i][j] + " ");
 			}
 			sb.append("\n");
 		}
-		
+
 		System.out.println(sb);
 	}
 }
