ebb96f092749939e722c43d5f52f474954879742
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-13 10:18:28 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 680 ms, Memory: 63692 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -2,79 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static int[][] map;
-	public static boolean[][] visit;
-	public static int n;
-	public static int m;
-	public static int[] dx = {1,-1,0,0};
-	public static int[] dy = {0,0,1,-1};
-	public static void main(String[] args) throws Exception  {
-	
+	static int[][] map;
+	static int[][] dist;
+	static boolean[][] visit;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	static int n, m;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		
+		int startX = 0;
+		int startY = 0;
 		map = new int[n][m];
+		dist = new int[n][m];
 		visit = new boolean[n][m];
-		int startX=0; int startY=0;
-		
-		for(int i = 0; i<n; i++) {
+
+		for(int i = 0; i<n; i++){
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<m; j++) {
+			for(int j = 0; j<m; j++){
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 2) {
-					startX = i; startY = j;
+				if(map[i][j] == 2){
+					startX = i;
+					startY = j;
 				}
 			}
 		}
-	
+
 		bfs(startX, startY);
-		checkInaccessible(startX, startY);
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<m; j++) {
-				System.out.print(map[i][j] + " ");
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<n; i++){
+			for(int j = 0; j<m; j++){
+				if(map[i][j] == 0){
+					sb.append(0 + " ");
+					continue;
+				}
+				if(!visit[i][j]){
+					sb.append(-1 + " ");
+					continue;
+				}
+				sb.append(dist[i][j] + " ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
-		
+
+		System.out.println(sb);
 	}
-	public static void bfs(int startX, int startY) {
+	public static void bfs(int x, int y){
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {startX,startY});
-		visit[startX][startY] = true;
-		map[startX][startY] = 0;
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int nowX = now[0];
-			int nowY = now[1];
-			
-			for(int i = 0; i<4; i++) {
-				int nextX = nowX + dx[i];
-				int nextY = nowY + dy[i];
-				if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
-					continue;
-				}
-				if(map[nextX][nextY] == 0 || visit[nextX][nextY] == true) {
-					continue;
-				}
-				q.add(new int[] {nextX,nextY});
-				visit[nextX][nextY] = true;
-				map[nextX][nextY] = map[nowX][nowY] + 1;
-			}
-		}
-	}
-	
-	public static void checkInaccessible(int startX, int startY) {
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<m; j++) {
-				if(map[i][j] == 1 && !visit[i][j]) {
-					map[i][j] = -1;
+		q.add(new int[] {x,y});
+		dist[x][y] = 0;
+		visit[x][y] = true;
+
+		while(!q.isEmpty()){
+			int[] poll = q.poll();
+			int curX = poll[0];
+			int curY = poll[1];
+
+
+			for(int i = 0; i<4; i++){
+				int nextX = curX + dx[i];
+				int nextY = curY + dy[i];
+
+				if(0 <= nextX && nextX < n && 0 <= nextY && nextY < m && !visit[nextX][nextY] && map[nextX][nextY] != 0){
+					dist[nextX][nextY] = dist[curX][curY] + 1;
+					q.add(new int[] {nextX, nextY});
+					visit[nextX][nextY] = true;
 				}
 			}
 		}
-		
+
 	}
 }
