99f2db6431403862cbc6affdfd9179bbcd7a61d2
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-14 19:22:19 +0900

    [Gold IV] Title: 인구 이동, Time: 572 ms, Memory: 297316 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -11,30 +11,31 @@ public class Main {
 	public static int[] dy = {0,0,1,-1};
 	public static boolean isUnion;
 	public static void main(String[] args) throws Exception  {
-	
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(st.nextToken());
 		l = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
-		
+
 		map = new int[n][n];
 		int count = 0;
-		
+
 		for(int i = 0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j<n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		while(true) {
 			visit = new boolean[n][n];
 			isUnion = false;
-			
+
 			for(int i = 0; i<n; i++) {
 				for(int j = 0; j<n; j++) {
+					if(visit[i][j]) continue;
 					findUnion(i,j);
 				}
 			}
@@ -44,12 +45,9 @@ public class Main {
 			count++;
 		}
 		System.out.println(count);
-		
+
 	}
 	public static void findUnion(int startX, int startY) {
-		if(visit[startX][startY] == true) {
-			return;
-		}
 		Queue<int[]> q = new LinkedList<>();
 		Queue<int[]> union = new LinkedList<>();
 		q.offer(new int[] {startX, startY});
@@ -57,21 +55,20 @@ public class Main {
 		union.offer(new int[] {startX,startY});
 		int sum = 0;
 		sum = sum + map[startX][startY];
-		
+
 		while(!q.isEmpty()) {
-			int now[] = q.poll();
+			int[] now = q.poll();
 			int nowX = now[0];
 			int nowY = now[1];
-			
+
 			for(int i = 0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				if(nextX < 0 || nextY < 0 || nextX >=n || nextY >=n) {
 					continue;
 				}
-				if(visit[nextX][nextY] == true) {
+				if(visit[nextX][nextY])
 					continue;
-				}
 				int dif = Math.abs(map[nextX][nextY] - map[nowX][nowY]);
 				if(dif>=l && dif<=r) {
 					isUnion = true;
@@ -84,7 +81,7 @@ public class Main {
 		}
 		popRelocation(union, sum);
 	}
-	
+
 	public static void popRelocation(Queue<int[]> union, int sum) {
 		int size = union.size();
 		int pop = sum/size;
