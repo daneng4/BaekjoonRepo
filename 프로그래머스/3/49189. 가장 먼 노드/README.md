f1e67ceb95d089052a8d43c002251fd462c06ede
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-18 00:27:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 37.36 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 52.86 ms
+메모리: 115 MB, 시간: 37.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 18일 23:41:47
+2025년 05월 18일 00:27:32
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,67 @@
 import java.util.*;
+import java.io.*;
+
 class Solution {
-    static boolean [] visit;
-    static ArrayList<Integer>[] list;
-    static int[] far;
+    static boolean[] visit;
+    static ArrayList<Integer>[] map;
+    static int[] dist;
+    static class Node{
+        int node;
+        int dist;
+        Node(int node, int dist){
+            this.node = node;
+            this.dist = dist;
+        }
+    }
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        
         visit = new boolean[n+1];
-        list = new ArrayList[n+1];
-        far = new int[n+1];
+        map = new ArrayList[n+1];
+        dist = new int[n+1];
         
         for(int i = 1; i<=n; i++){
-            list[i] = new ArrayList<>();
+            map[i] = new ArrayList<>();
         }
         
-        for(int[] e : edge){
-            list[e[0]].add(e[1]);
-            list[e[1]].add(e[0]);
+        for(int i = 0; i<edge.length; i++){
+            int a = edge[i][0]; 
+            int b = edge[i][1];
+            map[a].add(b);
+            map[b].add(a);
         }
         
-        solve(1);
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        bfs();
+        
         int max = 0;
-        for(int i = 1; i<=n; i++){
-            max = Math.max(max, far[i]);
+        for(int i = 1; i<dist.length; i++){
+            max = Math.max(max, dist[i]);
         }
-        for(int i = 1; i<=n; i++){
-            if(far[i] == max)
-                answer++;
+        for(int i = 1; i<dist.length; i++){
+            if(max == dist[i]) answer+=1;
         }
         
         return answer;
     }
-    public static void solve(int start){
-        Queue<int[]> q = new LinkedList<>();
-        visit[start] = true;
-        q.add(new int[] {start, 0});
+    public static void bfs(){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(1, 0));
+        visit[1] = true;
         
         while(!q.isEmpty()){
-            int[] poll = q.poll();
-            int now = poll[0];
-            int count = poll[1];
-            far[now] = count;
+            Node cur = q.poll();
+            dist[cur.node] = Math.min(dist[cur.node], cur.dist);
             
-            for(int next : list[now]){
+            for(int next : map[cur.node]){
                 if(visit[next]) continue;
-                q.add(new int[] {next, count + 1});
+                
+                q.add(new Node(next, cur.dist + 1));
                 visit[next] = true;
             }
         }
+        
         
     }
 }
