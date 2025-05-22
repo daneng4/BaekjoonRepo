f5e08d5eb882f790c5e74383398ecef38fcc888f
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-22 13:48:08 +0900

    [Gold IV] Title: 키 순서, Time: 560 ms, Memory: 37412 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,36 +1,34 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n,m;
 	static boolean[][] visit;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
 		visit = new boolean[n+1][n+1];
-		
-		
-		for(int i = 0; i<m; i++) {
+
+		for(int i = 0; i<m; i++){
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
 			visit[a][b] = true;
 		}
-		
-		for(int k = 1; k<=n; k++) {
-			for(int i = 1; i<=n; i++) {
-				for(int j = 1; j<=n; j++) {
-					if(visit[i][k] && visit[k][j])
+
+		for(int k= 1; k<=n; k++){
+			for(int i = 1; i<=n; i++){
+				for(int j = 1; j<=n; j++){
+					if(visit[i][k] && visit[k][j]){
 						visit[i][j] = true;
+					}
 				}
 			}
 		}
-		
+
 		int[] count = new int[n+1];
 		for(int i = 1; i<=n; i++) {
 			for(int j = 1; j<=n; j++) {
@@ -38,12 +36,13 @@ public class Main {
 					count[i]++;
 			}
 		}
+
 		int result = 0;
 		for(int i = 1; i<=n; i++) {
 			if(count[i] == n-1)
 				result++;
 		}
-		System.out.println(result);
-	}	
-}
 
+		System.out.println(result);
+	}
+}
