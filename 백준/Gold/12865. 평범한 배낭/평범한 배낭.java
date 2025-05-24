ce7042646a3c7fa4dc4cee37829b352d3de7bcfc
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-24 15:04:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53788 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,14 +1,13 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		
 		int[] w = new int[n+1];
 		int[] v = new int[n+1];
 		int[][] dp = new int[n+1][k+1];
@@ -21,14 +20,10 @@ public class Main {
 		
 		for(int i = 1; i<=n; i++) {
 			for(int j = 1; j<=k; j++) {
-				// 만약 현재 무게가 현재 감당할 수 있는 무게보다 크면
-				// 이전 무게의 value를 가져온다 => 더이상 가방에 담을 수 없음
-				if(w[i] > j)
-					dp[i][j] = dp[i-1][j];
-				// 뭔가 더 담을 수 있다면 현재 무게와 동일한 이전 무게에 현재 무게, 현재 value를
-				// 더한 것을 비교하여 큰 값을 저장한다
-				else
+				if(w[i] - j > 0) dp[i][j] = dp[i-1][j];
+				else {
 					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+				}
 			}
 		}
 		
