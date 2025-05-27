55b1d9a03edbc3f854ab03d7a166c1e8bcd3e6e9
김은서 <89981466+daneng4@users.noreply.github.com>
2025-05-27 14:48:33 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 16496 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,30 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int n = Integer.parseInt(br.readLine());
-		int[] num = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<n; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
+
+		int[] arr = new int[n];
 		int[] dp = new int[n];
-		for(int i = 0; i<n; i++) {
-			dp[i] = 1;
-			for(int j = 0; j<i; j++) {
-				if(num[i] > num[j] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j]+1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.fill(dp, 1);
+		for(int i = 0; i<n; i++){
+			int cur = arr[i];
+			for(int j = 0; j<i; j++){
+				int ex = arr[j];
+				if(ex < cur){
+					dp[i] = Math.max(dp[i], dp[j] + 1);
 				}
 			}
 		}
-		
-		int max = -1;
-		for(int i = 0; i<n; i++) {
+
+		int max = 0;
+		for(int i = 0; i<n; i++){
 			max = Math.max(max, dp[i]);
 		}
+
 		System.out.println(max);
 	}
 }
