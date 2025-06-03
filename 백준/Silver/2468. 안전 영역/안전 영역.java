a25de7b9088008b6c4b42f454b1e6da97106f29f
김은서 <89981466+daneng4@users.noreply.github.com>
2025-06-03 19:14:18 +0900

    [Silver I] Title: 안전 영역, Time: 280 ms, Memory: 54648 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,86 +1,72 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	static int[][] map;
-	static int n;
-	static int answer = 1;
+	static boolean[][] drownArea;
 	static int[] dx = {1,-1,0,0};
 	static int[] dy = {0,0,1,-1};
-    public static void main(String args[]) throws Exception  {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	n = Integer.parseInt(br.readLine());
-    	map = new int[n][n];
-    	
-    	int minH = Integer.MAX_VALUE;
-    	int maxH = 0;
-    	
-    	for(int i = 0; i<n; i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine());
-    		for(int j = 0; j<n; j++) {
-    			map[i][j] = Integer.parseInt(st.nextToken());
-    			minH = Math.min(minH, map[i][j]);
-    			maxH = Math.max(maxH, map[i][j]);
-    		}
-    	}
-    	
-    	for(int i = minH; i<=maxH; i++) {
-    		boolean[][] water = getSafeArea(i);
-    		int count = 0;
-    		
-    		for(int j = 0; j<n; j++) {
-    			for(int k = 0; k<n; k++) {
-    				if(!water[j][k]) {
-    					bfs(j,k, water);   
-    					count+=1;
-    				}
-    			}
-    		}
-    		
-    		answer = Math.max(answer, count);
-    	}
-    	
-    	System.out.println(answer);
-    }
-    
-    public static boolean[][] getSafeArea(int h) {
-    	boolean[][] visit = new boolean[n][n];
-    	
-    	for(int i = 0; i<n; i++) {
-    		for(int j = 0; j<n; j++) {
-    			if(map[i][j] <= h)
-    				visit[i][j] = true;
-    		}
-    	}
-    	
-    	return visit;
-    }
-    
-    public static void bfs(int x, int y, boolean[][] water) {
-    	Queue<int[]> q = new LinkedList<>();
-    	q.add(new int[] {x,y});
-    	water[x][y] = true;
-    	
-    	while(!q.isEmpty()) {
-    		int[] cur = q.poll();
-    		int curx = cur[0]; 
-    		int cury = cur[1];
-    		
-    		for(int i = 0; i<4; i++) {
-    			int nextx = curx + dx[i];
-    			int nexty = cury + dy[i];
-    			
-    			if(nextx < 0 || nextx >= n || nexty < 0 || nexty >= n)
-    				continue;
-    			if(water[nextx][nexty])
-    				continue;
-    			
-    			q.add(new int[] {nextx, nexty});
-    			water[nextx][nexty] = true;
-    		}
-    	}
-    	
-    }
+	static int n;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		map = new int[n][n];
+
+		int max = 0;
+		for(int i = 0; i<n; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<n; j++){
+				map[i][j] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, map[i][j]);
+			}
+		}
+
+		int maxArea = 0;
+		for(int i = 1; i <= max; i++){
+			drown(i);
+			int area = 0;
+			for(int j = 0; j<n; j++){
+				for(int k = 0; k<n; k++){
+					if(drownArea[j][k]) continue;
+					bfs(j,k);
+					area+=1;
+				}
+			}
+			if(area == 0)
+				area = 1;
+			maxArea = Math.max(maxArea, area);
+		}
+
+		System.out.println(maxArea);
+	}
+	public static void drown(int level){
+		drownArea = new boolean[n][n];
+		for(int i = 0; i<n; i++){
+			for(int j = 0; j<n; j++){
+				if(map[i][j] <= level)
+					drownArea[i][j] = true;
+			}
+		}
+	}
+	public static void bfs(int x, int y){
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x,y});
+		drownArea[x][y] = true;
+
+		while(!q.isEmpty()){
+			int[] cur = q.poll();
+			int cx = cur[0]; int cy = cur[1];
+
+			for(int i = 0; i<4; i++){
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+				if(drownArea[nx][ny]) continue;
+
+				q.add(new int[] {nx, ny});
+				drownArea[nx][ny] = true;
+			}
+		}
+
+	}
 }
