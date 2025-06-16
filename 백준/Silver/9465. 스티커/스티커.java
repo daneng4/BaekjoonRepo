2b1c4aa039b406e242b2121aa1ffeba90140eedb
김은서 <89981466+daneng4@users.noreply.github.com>
2025-06-16 13:09:36 +0900

    [Silver I] Title: 스티커, Time: 680 ms, Memory: 110580 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,45 +1,32 @@
-
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	public static void main(String[] args) throws Exception  {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for(int tc = 0; tc<T; tc++) {
+
+		while(T-->0) {
 			int n = Integer.parseInt(br.readLine());
-			int[][] score = new int[2][n];
-			int[][] dp = new int[2][n];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			
-			for(int i = 0; i<n; i++) {
-				score[0][i] = Integer.parseInt(st.nextToken());
-				score[1][i] = Integer.parseInt(st2.nextToken());
-			}
-			
-			dp[0][0] = score[0][0];
-			dp[1][0] = score[1][0];
-			int max = Math.max(dp[0][0], dp[1][0]);
-			
-			for(int i = 1; i<n; i++) {
-				if(i==1) {
-					dp[0][i] = dp[1][0] + score[0][1];
-					dp[1][i] = dp[0][0] + score[1][1];
-					max = Math.max(dp[0][i], dp[1][i]);
-					continue;
+			int[][] stickers = new int[2][n + 1];
+			int[][] dp = new int[2][n + 1];
+
+			for(int i = 0; i<2; i++){
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j = 1; j<=n; j++){
+					stickers[i][j] = Integer.parseInt(st.nextToken());
 				}
-				dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + score[0][i];
-				dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + score[1][i];
-                max = Math.max(max, Math.max(dp[0][i], dp[1][i]));
 			}
-			sb.append(max + "\n");
+
+			dp[0][1] = stickers[0][1];
+			dp[1][1] = stickers[1][1];
+
+			for(int i = 2; i<=n; i++){
+				dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + stickers[0][i];
+				dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + stickers[1][i];
+			}
+
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 		}
-		System.out.println(sb);
 	}
 }
-
