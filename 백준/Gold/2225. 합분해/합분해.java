2726f9aabf9ea8175d3f454d70287572b067ccb4
김은서 <89981466+daneng4@users.noreply.github.com>
2025-08-16 15:56:28 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,30 +1,31 @@
-
 import java.io.*;
 import java.util.*;
 
-/*
- * 0부터 N까지의 정수 중 K개를 더해서 그 합이 N이 되는 경우의 수
- */
-
 public class Main {
-	public static void main(String[] args) throws Exception  {
+	static int n;
+	static int k;
+	static int[][] dp;
+	public static void main(String args[]) throws Exception  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[k+1][n+1]; // dp[a][b] -> a개를 더해서 b를 만드는 경우의 수
-		Arrays.fill(dp[1] , 1);
-		for(int i = 1; i <= k; i++) {
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		dp = new int[k+1][n+1]; // dp[a][b] : a개를 더해 b를 만드는 경우의 수를 의미
+		
+		Arrays.fill(dp[1], 1); // 1개를 사용해서 b를 만드는 경우는 자기 자신이 존재함
+		
+		for(int i = 1; i<=k; i++) {
 			dp[i][0] = 1;
 		}
+		
 		for(int i = 2; i<=k; i++) {
-			for(int j = 1; j<=n; j++) {
+			for(int j = 1; j<= n; j++) {
 				dp[i][j] = dp[i-1][j] + dp[i][j-1];
-				dp[i][j] %= 1000000000;
+				dp[i][j] = dp[i][j] % 1000000000;
 			}
 		}
+		
 		System.out.println(dp[k][n]);
 	}
 }
-
