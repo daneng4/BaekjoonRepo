2a04f652f641cc3d6439aa960f7803a3eff7508a
김은서 <89981466+daneng4@users.noreply.github.com>
2025-12-01 20:32:38 +0900

    [Silver I] Title: 트럭, Time: 120 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -24,35 +24,32 @@ public class Main {
 
         int passed = 0; // 다리를 건너간 트럭의 개수를 세는 변수
         int currentL = 0; // 현재 다리 하중
-        int time = 0;
+        int time = 0; // 시간
 
-        while(true){
+        while (passed != n) {
             // 트럭이 전부 지나갔다면 종료
-            if(passed == n){
-                break;
-            }
 
             // 다리의 맨 앞이 빠져나간다. 빠져나가며 무게도 갱신된다.
             int out = bridge.poll();
-            if(out > 0)
+            if (out > 0)
                 passed++;
             currentL -= out;
 
             // 트럭 투입 여부를 결정한다. 더이상 투입될 트럭이 없다면 add 0
-            if(!truck.isEmpty()){
+            if (!truck.isEmpty()) {
 
                 // 다음 투입될 트럭 + 현재 다리 하중이 최대 하중 이하라면 가능
-                if(currentL + truck.peek() <= L){
+                if (currentL + truck.peek() <= L) {
                     int currentTruck = truck.poll();
                     bridge.add(currentTruck);
 
                     // 무게를 갱신해준다.
                     currentL += currentTruck;
-                }else{
+                } else {
                     bridge.add(0);
                 }
 
-            }else{
+            } else {
                 bridge.add(0);
             }
 
