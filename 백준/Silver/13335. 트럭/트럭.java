81f2d00c1a04e0ac3336046532b38a324bb82f59
김은서 <89981466+daneng4@users.noreply.github.com>
2025-12-01 20:27:02 +0900

    [Silver I] Title: 트럭, Time: 120 ms, Memory: 15312 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -2,55 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static Queue<Integer> bridge;
-	private static Queue<Integer> ends;
-	private static int l;
-	public static void main(String args[]) throws Exception  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken()); // 건널 트럭 개수
-		int w = Integer.parseInt(st.nextToken()); // 동시에 건널 수 있는 트럭 수
-		l = Integer.parseInt(st.nextToken()); // 다리의 최대 하중
-		
-		Queue<Integer> trucks = new LinkedList<>();
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<n; i++) {
-			trucks.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		bridge = new LinkedList<>(); // 다리
-		ends = new LinkedList<>(); // 건넌 트럭 담을 큐
-		for(int i = 0; i<w; i++) {
-			bridge.add(0);
-		}
-		
-		int time = 0;
-		// 트럭이 전부 건너기 전까지 반복하여 수행
-		while(ends.size() != n) {
-			time += 1;
-			int end = bridge.poll();
-			if(end > 0) {
-				ends.add(end);
-			}
-			if(checkWeight(bridge, trucks.peek())) {
-				bridge.add(trucks.poll());
-				trucks.add(0);
-			}else {
-				bridge.add(0);
-			}
-		}
-		
-		System.out.println(time);
-	}
-	private static boolean checkWeight(Queue<Integer> bridge, int truck) {
-		int weight = 0;
-		for(int w : bridge) {
-			weight += w;
-		}
-		
-		if(weight + truck > l)
-			return false;
-		return true;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 다리를 건너는 트럭의 수
+        int w = Integer.parseInt(st.nextToken()); // 다리의 길이
+        int L = Integer.parseInt(st.nextToken()); // 다리의 최대하중
+
+        st = new StringTokenizer(br.readLine());
+        Queue<Integer> truck = new LinkedList<>();
+        Queue<Integer> bridge = new LinkedList<>();
+
+        for(int i = 0; i<n; i++){
+            truck.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for(int i = 0; i<w; i++){
+            bridge.add(0);
+        }
+
+        int passed = 0; // 다리를 건너간 트럭의 개수를 세는 변수
+        int currentL = 0; // 현재 다리 하중
+        int time = 0;
+
+        while(true){
+            // 트럭이 전부 지나갔다면 종료
+            if(passed == n){
+                break;
+            }
+
+            // 다리의 맨 앞이 빠져나간다. 빠져나가며 무게도 갱신된다.
+            int out = bridge.poll();
+            if(out > 0)
+                passed++;
+            currentL -= out;
+
+            // 트럭 투입 여부를 결정한다. 더이상 투입될 트럭이 없다면 add 0
+            if(!truck.isEmpty()){
+
+                // 다음 투입될 트럭 + 현재 다리 하중이 최대 하중 이하라면 가능
+                if(currentL + truck.peek() <= L){
+                    int currentTruck = truck.poll();
+                    bridge.add(currentTruck);
+
+                    // 무게를 갱신해준다.
+                    currentL += currentTruck;
+                }else{
+                    bridge.add(0);
+                }
+
+            }else{
+                bridge.add(0);
+            }
+
+            time++;
+        }
+
+        System.out.println(time);
+    }
 }
