7412b755aac2b9a13365ed9f5acc2ad13735a970
김은서 <89981466+daneng4@users.noreply.github.com>
2025-12-08 20:39:15 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,37 +1,27 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int[] card = new int[n+1];
-		int[] dp = new int[n+1];
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
+		int[] p = new int[N];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for(int i = 1; i<=n; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		// 카드 n개를 구매 할때 최대 비용
-		// 카드 개수는 card 배열의 인덱스, 배열의 값이 i장당 가격을 의미
 
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1; j<=i; j++) {
-				dp[i] = Math.max(dp[i], dp[i-j] + card[j]);
+		
+		// i : 현재까지 살 수 있는 최대 카드 개수
+		for(int i = 1; i<=N; i++) {
+			for(int j = 0; j<i; j++) {
+				dp[i] = Math.max(dp[i], p[i-j-1] + dp[j]);
 			}
 		}
 		
-		System.out.println(dp[n]);
-		
+		System.out.println(dp[N]);
 	}
-
 }
-
-
-
-
