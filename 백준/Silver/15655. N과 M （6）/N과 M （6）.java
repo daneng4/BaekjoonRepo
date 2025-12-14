3163ac7cccb526a55830bec2dc1635d66ce98d35
김은서 <89981466+daneng4@users.noreply.github.com>
2025-12-14 11:44:11 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -2,50 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
+    static int N, M;
     static int[] arr;
-    static StringBuilder sb1;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         arr = new int[N];
+        selected = new int[M];
 
-        sb1 = new StringBuilder();
         st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        if(N == M){
-            StringBuilder sb2 = new StringBuilder();
-            for(int n : arr)
-                sb2.append(n + " ");
+        dfs(0, 0);
 
-            System.out.println(sb2);
-            return;
-        }
-
-
-        solve(0, 0, "");
-
-        System.out.println(sb1);
+        System.out.print(sb);
     }
 
-    private static void solve(int len, int picked, String str){
-        if(len == M){
-            sb1.append(str + "\n");
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
             return;
         }
 
-        for(int i = picked; i<N; i++){
-            solve(len + 1, i + 1, str + arr[i] + " ");
+        for (int i = start; i < N; i++) {
+            selected[depth] = arr[i];
+            dfs(depth + 1, i + 1);
         }
     }
 }
