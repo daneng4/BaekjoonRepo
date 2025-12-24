555538f324fa337ea46804c6cebf9df41167c6dc
김은서 <89981466+daneng4@users.noreply.github.com>
2025-12-24 11:29:28 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,40 @@
-
-import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
-	static int n,m;
-	static StringBuilder sb = new StringBuilder();
-	static boolean[] visit;
-	public static void main(String[] args) throws IOException {
+	static int N;
+	static int M;
+	static int[] arr;
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		n = Integer.parseInt(st.nextToken()); // 1~n까지 자연수
-		m = Integer.parseInt(st.nextToken()); // m자리
-		visit = new boolean[n+1];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		solve("", 0, 1);
+		arr = new int[N+1];
+		sb = new StringBuilder();
+		
+		solve(1, 1);
+		
 		System.out.println(sb);
 	}
-	public static void solve(String str, int depth, int idx) {
-		if(depth == m) {
-			sb.append(str).append("\n");
+	
+	public static void solve(int depth, int picked) {
+		if(depth == M+1) {
+			
+			for(int i = 1; i<=M; i++){
+				sb.append(arr[i] + " ");
+			}
+			
+			sb.append("\n");
 			return;
 		}
 		
-		for(int i = idx; i<=n; i++) {
-			if(visit[i])
-				continue;
-			visit[i] = true;
-			solve(str.concat(i + " "), depth+1, i);
-			visit[i] = false;
+		for(int i = picked; i<=N; i++) {
+			arr[depth] = i;
+			solve(depth+1, i+1);
 		}
-		
 	}
 }
