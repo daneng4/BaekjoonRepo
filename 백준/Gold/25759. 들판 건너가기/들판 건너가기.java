f8de835fb24073db960ad837c46d79b2ba153386
김은서 <89981466+daneng4@users.noreply.github.com>
2026-01-08 15:12:21 +0900

    [Gold IV] Title: 들판 건너가기, Time: 336 ms, Memory: 26028 KB -BaekjoonHub

---
+++ 백준/Gold/25759. 들판 건너가기/들판 건너가기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[101];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int first = Integer.parseInt(st.nextToken());
+
+		Arrays.fill(dp, Integer.MIN_VALUE);
+		
+		dp[first] = 0; // 꽃을 하나만 고른 경우, 인접한 꽃이 없어서 0
+		
+		for(int i = 1; i<N; i++) {
+			int cur = Integer.parseInt(st.nextToken());
+			int best = Integer.MIN_VALUE;
+			
+			for(int pre = 1; pre<=100; pre++) {
+				if(dp[pre] != Integer.MIN_VALUE) {
+					int val = dp[pre] + (cur - pre) * (cur - pre);
+					best = Math.max(val, best);
+				}
+			}
+			
+			dp[cur] = Math.max(dp[cur], best);
+		}
+		
+		int answer = 0;
+		for(int i = 1; i<=100; i++) {
+			answer = Math.max(answer, dp[i]);
+		}
+		
+		System.out.println(answer);
+	}
+}
