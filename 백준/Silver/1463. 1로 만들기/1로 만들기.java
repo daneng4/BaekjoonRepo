7a190920a30ecabe144dc35d8d1f59c3dae21f3f
김은서 <89981466+daneng4@users.noreply.github.com>
2026-04-16 11:58:48 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18244 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,31 +1,26 @@
-
 import java.io.*;
 import java.util.*;
-class Main {
+
+public class Main {
 	static Integer[] dp;
-	public static void main(String[] args) throws Exception {
+	public static void main(String args[]) throws Exception  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int x = Integer.parseInt(br.readLine());
 		
-		dp = new Integer[n+1];
+		dp = new Integer[x+1];
 		dp[0] = dp[1] = 0;
-		System.out.println(rec(n));
-	}
-	public static int rec(int n) {
-		if(dp[n] == null) {
-			if(n%6==0) {
-				dp[n] = Math.min(rec(n-1), Math.min(rec(n/3), rec(n/2))) + 1;
+		
+		for(int i = 2; i <= x; i++) {
+			dp[i] = dp[i-1] + 1;
+
+			if(i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
 			}
-			else if(n%3 ==0) {
-				dp[n] = Math.min(rec(n/3), rec(n-1)) + 1;
-			}
-			else if(n%2 == 0) {
-				dp[n] = Math.min(rec(n/2), rec(n-1)) + 1;
-			}
-			else {
-				dp[n] = rec(n-1) + 1;
+			if(i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
 			}
 		}
-		return dp[n];
+		
+		System.out.println(dp[x]);
 	}
 }
